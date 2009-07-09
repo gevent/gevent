b2df0a2fc1d77a3ef75d7a6a7b4962121164e733
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-09 18:37:09 +0700

    wsgi_test.py: monkey patch ssl too

--- greentest/wsgi_test.py
+++ greentest/wsgi_test.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 from gevent import monkey
-monkey.patch_socket()
+monkey.patch_all(thread=False)
 
 import cgi
 import os
