d7caf08f4a1311eeb60c09e021fc9da605897c45
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-14 13:34:34 +0700

    add a new test case for ssl by Cesar Alaniz

---
+++ greentest/test__socket_ssl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from gevent import monkey; monkey.patch_all()
+
+import unittest
+import httplib
+
+class AmazonHTTPSTests(unittest.TestCase):
+
+    def test_amazon_response(self):
+        conn = httplib.HTTPConnection('sdb.amazonaws.com')
+        conn.debuglevel = 1
+        conn.request('GET', '/')
+        resp = conn.getresponse()
+
+if __name__ == "__main__":
+    unittest.main()
+    
