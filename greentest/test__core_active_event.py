039674667ac678433bbeeb54026c35ad34ad3b2e
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-19 10:58:52 +0700

    add test__core_active_event.py

---
+++ greentest/test__core_active_event.py
@@ -0,0 +1,20 @@
+import gevent
+
+called = []
+
+def f():
+    called.append(1)
+
+x = gevent.core.active_event(f)
+assert x.pending == 1, x.pending
+gevent.sleep(0)
+assert x.pending == 0, x.pending
+assert called == [1], called
+
+x = gevent.core.active_event(f)
+assert x.pending == 1, x.pending
+x.cancel()
+assert x.pending == 0, x.pending
+gevent.sleep(0)
+assert called == [1], called
+assert x.pending == 0, x.pending
