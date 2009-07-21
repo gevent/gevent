ca51288ff97a51f36f76edc8df0d8fb298e74d5b
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-21 11:37:22 +0700

    test__httpd.py: update not to use a specific port

--- greentest/test__greenness.py
+++ greentest/test__greenness.py
@@ -32,13 +32,11 @@ import urllib2
 import BaseHTTPServer
 from gevent import spawn, kill
 
-port = 18341
-
 def start_http_server():
-    server_address = ('', port)
+    server_address = ('', 0)
     BaseHTTPServer.BaseHTTPRequestHandler.protocol_version = "HTTP/1.0"
     httpd = BaseHTTPServer.HTTPServer(server_address, BaseHTTPServer.BaseHTTPRequestHandler)
-    sa = httpd.socket.getsockname()
+    #sa = httpd.socket.getsockname()[1]
     #print "Serving HTTP on", sa[0], "port", sa[1], "..."
     httpd.request_count = 0
     def serve():
@@ -49,21 +47,21 @@ def start_http_server():
 class TestGreenness(greentest.TestCase):
 
     def setUp(self):
-        self.gthread, self.server = start_http_server()
-        #print 'Spawned the server'
+        self.gthread, self.httpd = start_http_server()
 
     def tearDown(self):
-        self.server.server_close()
+        self.httpd.server_close()
         kill(self.gthread)
 
     def test_urllib2(self):
-        self.assertEqual(self.server.request_count, 0)
+        self.assertEqual(self.httpd.request_count, 0)
+        port = self.httpd.socket.getsockname()[1]
         try:
             urllib2.urlopen('http://127.0.0.1:%s' % port)
             assert False, 'should not get there'
         except urllib2.HTTPError, ex:
             assert ex.code == 501, `ex`
-        self.assertEqual(self.server.request_count, 1)
+        self.assertEqual(self.httpd.request_count, 1)
 
 if __name__ == '__main__':
     test_support.run_unittest(TestGreenness)
