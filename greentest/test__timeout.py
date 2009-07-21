684f07cc13fd36ade4290bef238da39e149fddd1
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-21 11:36:50 +0700

    add test__timeout.py

---
+++ greentest/test__timeout.py
@@ -0,0 +1,33 @@
+import greentest
+import gevent
+DELAY = 0.01
+
+
+class Test(greentest.TestCase):
+
+    def test_direct_raise_class(self):
+        try:
+            raise gevent.Timeout
+        except gevent.Timeout, t:
+            assert not t.pending, repr(t)
+
+    def test_direct_raise_instance(self):
+        timeout = gevent.Timeout()
+        try:
+            raise timeout
+        except gevent.Timeout, t:
+            assert timeout is t, (timeout, t)
+            assert not t.pending, repr(t)
+
+    def test_with_timeout(self):
+        self.assertRaises(gevent.Timeout, gevent.with_timeout, DELAY, gevent.sleep, DELAY*2)
+        X = object()
+        r = gevent.with_timeout(DELAY, gevent.sleep, DELAY*2, timeout_value=X)
+        assert r is X, (r, X)
+        r = gevent.with_timeout(DELAY*2, gevent.sleep, DELAY, timeout_value=X)
+        assert r is None, r
+
+
+if __name__=='__main__':
+    greentest.main()
+
