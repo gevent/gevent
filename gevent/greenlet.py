464b0474c83d5e75244b7e8b61840a305e49bdd7
Denis Bilenko <denis.bilenko@gmail.com>
2009-07-29 12:57:36 +0700

    greenlet.py: rearrange classes - put user visible stuff at the top and internal classes at the bottom

--- gevent/greenlet.py
+++ gevent/greenlet.py
@@ -6,14 +6,16 @@ from gevent import core
 
 
 __all__ = ['getcurrent',
-           'Timeout',
+           'sleep',
            'spawn',
            'spawn_later',
            'kill',
            'killall',
-           'sleep',
-           'signal',
+           'join',
+           'joinall',
+           'Timeout',
            'with_timeout',
+           'signal',
            'fork']
 
 
@@ -32,6 +34,26 @@ thread = __import__('thread')
 threadlocal = thread._local
 _threadlocal = threadlocal()
 _threadlocal.Hub = None
+_original_fork = os.fork
+
+
+def sleep(seconds=0):
+    """Yield control to another eligible coroutine until at least *seconds* have
+    elapsed.
+
+    *seconds* may be specified as an integer, or a float if fractional seconds
+    are desired. Calling sleep with *seconds* of 0 is the canonical way of
+    expressing a cooperative yield. For example, if one is looping over a
+    large list performing an expensive calculation without calling any socket
+    methods, it's a good idea to call ``sleep(0)`` occasionally; otherwise
+    nothing else will run.
+    """
+    hub = get_hub()
+    t = core.timer(seconds, greenlet.getcurrent().switch)
+    try:
+        hub.switch()
+    finally:
+        t.cancel()
 
 
 def _switch_helper(function, args, kwargs):
@@ -63,77 +85,6 @@ def spawn_later(seconds, function, *args, **kwargs):
         g.parent = get_hub().greenlet
         core.timer(seconds, g.switch, *args)
         return g
-
-
-class Waiter(object):
-    """A low level synchronization class.
-
-    Wrapper around switch() and throw() calls that makes them safe:
-    a) switching will occur only if the waiting greenlet is executing wait()
-       method currently. Otherwise, switch() and throw() are no-ops.
-    b) any error raised in the greenlet is handled inside switch() and throw()
-
-    switch and throw methods must only be called from the mainloop greenlet.
-    wait must be called from a greenlet other than mainloop.
-    """
-    __slots__ = ['greenlet']
-
-    def __init__(self):
-        self.greenlet = None
-
-    def __repr__(self):
-        if self.waiting:
-            waiting = ' waiting'
-        else:
-            waiting = ''
-        return '<%s at %s%s greenlet=%r>' % (type(self).__name__, hex(id(self)), waiting, self.greenlet)
-
-    def __str__(self):
-        """
-        >>> print Waiter()
-        <Waiter greenlet=None>
-        """
-        if self.waiting:
-            waiting = ' waiting'
-        else:
-            waiting = ''
-        return '<%s%s greenlet=%s>' % (type(self).__name__, waiting, self.greenlet)
-
-    @property
-    def waiting(self):
-        return self.greenlet is not None
-
-    def switch(self, value=None):
-        """Wake up the greenlet that is calling wait() currently (if there is one).
-        Can only be called from Hub's greenlet.
-        """
-        assert greenlet.getcurrent() is get_hub().greenlet, "Can only use Waiter.switch method from the mainloop"
-        if self.greenlet is not None:
-            try:
-                self.greenlet.switch(value)
-            except:
-                traceback.print_exc()
-
-    def throw(self, *throw_args):
-        """Make greenlet calling wait() wake up (if there is a wait()).
-        Can only be called from Hub's greenlet.
-        """
-        assert greenlet.getcurrent() is get_hub().greenlet, "Can only use Waiter.switch method from the mainloop"
-        if self.greenlet is not None:
-            try:
-                self.greenlet.throw(*throw_args)
-            except:
-                traceback.print_exc()
-
-    def wait(self):
-        """Wait until switch() or throw() is called.
-        """
-        assert self.greenlet is None, 'This Waiter is already used by %r' % (self.greenlet, )
-        self.greenlet = greenlet.getcurrent()
-        try:
-            return get_hub().switch()
-        finally:
-            self.greenlet = None
 
 
 def _kill(greenlet, exception, waiter):
@@ -206,118 +157,6 @@ def _joinall(greenlets, polling_period=0.2):
 def joinall(greenlets, polling_period=0.2):
     """Wait for the greenlets to finish by polling their status"""
     return _joinall(list(greenlets), polling_period=polling_period)
-
-
-def sleep(seconds=0):
-    """Yield control to another eligible coroutine until at least *seconds* have
-    elapsed.
-
-    *seconds* may be specified as an integer, or a float if fractional seconds
-    are desired. Calling sleep with *seconds* of 0 is the canonical way of
-    expressing a cooperative yield. For example, if one is looping over a
-    large list performing an expensive calculation without calling any socket
-    methods, it's a good idea to call ``sleep(0)`` occasionally; otherwise
-    nothing else will run.
-    """
-    hub = get_hub()
-    t = core.timer(seconds, greenlet.getcurrent().switch)
-    try:
-        hub.switch()
-    finally:
-        t.cancel()
-
-
-def signal(signalnum, handler, *args, **kwargs):
-    def deliver_exception_to_MAIN():
-        try:
-            handler(*args, **kwargs)
-        except:
-            MAIN.throw(*sys.exc_info())
-    return core.signal(signalnum, deliver_exception_to_MAIN)
-
-
-def get_hub():
-    global _threadlocal
-    try:
-        return _threadlocal.hub
-    except AttributeError:
-        try:
-            hubtype = _threadlocal.Hub
-        except AttributeError:
-            # do not pretend to support multiple threads because it's not implemented properly by core.pyx
-            # this may change in the future, although currently I don't have a strong need for this
-            raise NotImplementedError('gevent is only usable from a single thread')
-        if hubtype is None:
-            hubtype = Hub
-        hub = _threadlocal.hub = hubtype()
-        return hub
-
-
-class Hub(object):
-
-    def __init__(self):
-        self.greenlet = Greenlet(self.run)
-        self.keyboard_interrupt_signal = None
-
-    def switch(self):
-        cur = getcurrent()
-        assert cur is not self.greenlet, 'Cannot switch to MAINLOOP from MAINLOOP'
-        switch_out = getattr(cur, 'switch_out', None)
-        if switch_out is not None:
-            try:
-                switch_out()
-            except:
-                traceback.print_exc()
-        if self.greenlet.dead:
-            self.greenlet = Greenlet(self.run)
-        return self.greenlet.switch()
-
-    def run(self):
-        if self.keyboard_interrupt_signal is None:
-            self.keyboard_interrupt_signal = signal(2, MAIN.throw, KeyboardInterrupt)
-        loop_count = 0
-        while True:
-            try:
-                result = core.dispatch()
-            except IOError, ex:
-                loop_count += 1
-                if loop_count > 15:
-                    raise
-                sys.stderr.write('Restarting gevent.core.dispatch() after an error [%s]: %s\n' % (loop_count, ex))
-                continue
-            if result==1:
-                raise DispatchExit('No events registered')
-            raise DispatchExit('dispatch() exited with code %s' % (result, ))
-
-
-class DispatchExit(Exception):
-    pass
-
-
-def _wait_helper(ev, evtype):
-    current, timeout_exc = ev.arg
-    if evtype & core.EV_TIMEOUT:
-        current.throw(timeout_exc)
-    else:
-        current.switch(ev)
-
-
-def wait_reader(fileno, timeout=-1, timeout_exc=_socket.timeout):
-    evt = core.read(fileno, _wait_helper, timeout, (getcurrent(), timeout_exc))
-    try:
-        returned_ev = get_hub().switch()
-        assert evt is returned_ev, (evt, returned_ev)
-    finally:
-        evt.cancel()
-
-
-def wait_writer(fileno, timeout=-1, timeout_exc=_socket.timeout):
-    evt = core.write(fileno, _wait_helper, timeout, (getcurrent(), timeout_exc))
-    try:
-        returned_ev = get_hub().switch()
-        assert evt is returned_ev, (evt, returned_ev)
-    finally:
-        evt.cancel()
 
 
 try:
@@ -485,10 +324,175 @@ def with_timeout(seconds, func, *args, **kwds):
         timeout.cancel()
 
 
-_original_fork = os.fork
+
+def signal(signalnum, handler, *args, **kwargs):
+    def deliver_exception_to_MAIN():
+        try:
+            handler(*args, **kwargs)
+        except:
+            MAIN.throw(*sys.exc_info())
+    return core.signal(signalnum, deliver_exception_to_MAIN)
+
 
 def fork():
     result = _original_fork()
     core.reinit()
     return result
+
+
+class Waiter(object):
+    """A low level synchronization class.
+
+    Wrapper around switch() and throw() calls that makes them safe:
+    a) switching will occur only if the waiting greenlet is executing wait()
+       method currently. Otherwise, switch() and throw() are no-ops.
+    b) any error raised in the greenlet is handled inside switch() and throw()
+
+    switch and throw methods must only be called from the mainloop greenlet.
+    wait must be called from a greenlet other than mainloop.
+    """
+    __slots__ = ['greenlet']
+
+    def __init__(self):
+        self.greenlet = None
+
+    def __repr__(self):
+        if self.waiting:
+            waiting = ' waiting'
+        else:
+            waiting = ''
+        return '<%s at %s%s greenlet=%r>' % (type(self).__name__, hex(id(self)), waiting, self.greenlet)
+
+    def __str__(self):
+        """
+        >>> print Waiter()
+        <Waiter greenlet=None>
+        """
+        if self.waiting:
+            waiting = ' waiting'
+        else:
+            waiting = ''
+        return '<%s%s greenlet=%s>' % (type(self).__name__, waiting, self.greenlet)
+
+    @property
+    def waiting(self):
+        return self.greenlet is not None
+
+    def switch(self, value=None):
+        """Wake up the greenlet that is calling wait() currently (if there is one).
+        Can only be called from Hub's greenlet.
+        """
+        assert greenlet.getcurrent() is get_hub().greenlet, "Can only use Waiter.switch method from the mainloop"
+        if self.greenlet is not None:
+            try:
+                self.greenlet.switch(value)
+            except:
+                traceback.print_exc()
+
+    def throw(self, *throw_args):
+        """Make greenlet calling wait() wake up (if there is a wait()).
+        Can only be called from Hub's greenlet.
+        """
+        assert greenlet.getcurrent() is get_hub().greenlet, "Can only use Waiter.switch method from the mainloop"
+        if self.greenlet is not None:
+            try:
+                self.greenlet.throw(*throw_args)
+            except:
+                traceback.print_exc()
+
+    def wait(self):
+        """Wait until switch() or throw() is called.
+        """
+        assert self.greenlet is None, 'This Waiter is already used by %r' % (self.greenlet, )
+        self.greenlet = greenlet.getcurrent()
+        try:
+            return get_hub().switch()
+        finally:
+            self.greenlet = None
+
+
+
+def get_hub():
+    global _threadlocal
+    try:
+        return _threadlocal.hub
+    except AttributeError:
+        try:
+            hubtype = _threadlocal.Hub
+        except AttributeError:
+            # do not pretend to support multiple threads because it's not implemented properly by core.pyx
+            # this may change in the future, although currently I don't have a strong need for this
+            raise NotImplementedError('gevent is only usable from a single thread')
+        if hubtype is None:
+            hubtype = Hub
+        hub = _threadlocal.hub = hubtype()
+        return hub
+
+
+class Hub(object):
+
+    def __init__(self):
+        self.greenlet = Greenlet(self.run)
+        self.keyboard_interrupt_signal = None
+
+    def switch(self):
+        cur = getcurrent()
+        assert cur is not self.greenlet, 'Cannot switch to MAINLOOP from MAINLOOP'
+        switch_out = getattr(cur, 'switch_out', None)
+        if switch_out is not None:
+            try:
+                switch_out()
+            except:
+                traceback.print_exc()
+        if self.greenlet.dead:
+            self.greenlet = Greenlet(self.run)
+        return self.greenlet.switch()
+
+    def run(self):
+        if self.keyboard_interrupt_signal is None:
+            self.keyboard_interrupt_signal = signal(2, MAIN.throw, KeyboardInterrupt)
+        loop_count = 0
+        while True:
+            try:
+                result = core.dispatch()
+            except IOError, ex:
+                loop_count += 1
+                if loop_count > 15:
+                    raise
+                sys.stderr.write('Restarting gevent.core.dispatch() after an error [%s]: %s\n' % (loop_count, ex))
+                continue
+            if result==1:
+                raise DispatchExit('No events registered')
+            raise DispatchExit('dispatch() exited with code %s' % (result, ))
+
+
+class DispatchExit(Exception):
+    pass
+
+
+def _wait_helper(ev, evtype):
+    current, timeout_exc = ev.arg
+    if evtype & core.EV_TIMEOUT:
+        current.throw(timeout_exc)
+    else:
+        current.switch(ev)
+
+
+def wait_reader(fileno, timeout=-1, timeout_exc=_socket.timeout):
+    evt = core.read(fileno, _wait_helper, timeout, (getcurrent(), timeout_exc))
+    try:
+        returned_ev = get_hub().switch()
+        assert evt is returned_ev, (evt, returned_ev)
+    finally:
+        evt.cancel()
+
+
+def wait_writer(fileno, timeout=-1, timeout_exc=_socket.timeout):
+    evt = core.write(fileno, _wait_helper, timeout, (getcurrent(), timeout_exc))
+    try:
+        returned_ev = get_hub().switch()
+        assert evt is returned_ev, (evt, returned_ev)
+    finally:
+        evt.cancel()
+
 
