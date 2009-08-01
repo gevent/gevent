cfca6adca7f4c529d08cf5c3df8f33b54c93ded5
Denis Bilenko <denis.bilenko@gmail.com>
2009-08-01 18:14:26 +0700

    add test__backdoor.py

---
+++ greentest/test__backdoor.py
@@ -0,0 +1,24 @@
+import greentest
+import gevent
+from gevent import socket
+from gevent import backdoor
+
+class Test(greentest.TestCase):
+
+    def test(self):
+        g = gevent.spawn(backdoor.backdoor_server, socket.tcp_listener(('127.0.0.1', 7891)))
+        gevent.sleep(0.1)
+        s = socket.create_connection(('127.0.0.1', 7891))
+        f = s.makefile()
+        while True:
+            line = gevent.with_timeout(0.1, f.readline, timeout_value=None)
+            if line is None:
+                break
+        s.sendall('2+2\r\n')
+        l = f.readline()
+        assert l.strip() == '4', repr(l)
+        gevent.kill(g, block=True)
+
+
+if __name__ == '__main__':
+    greentest.main()
