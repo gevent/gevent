81f3f81d5f3d0893f1842d88cea249d8ce0c6e10
Denis Bilenko <denis.bilenko@gmail.com>
2009-08-24 12:03:04 +0700

    add test__benchmarks.py

---
+++ greentest/test__benchmarks.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import glob
+
+modules = set()
+
+for path in glob.glob('bench_*.py'):
+    modules.add(path)
+
+assert modules
+
+error = 0
+
+for path in modules:
+    print path
+    sys.stdout.flush()
+    res = os.system('%s %s all' % (sys.executable, path))
+    if res:
+        error = 1
+        print path, 'failed'
+    print '-----'
+
+if error:
+    sys.exit(1)
+
