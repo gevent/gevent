405d689cb9026e93de28502d5a6797ac3f2f58c0
Denis Bilenko <denis.bilenko@gmail.com>
2009-09-08 18:32:48 +0700

    update examples/wsgiserver.py to use wsgi2

--- examples/wsgiserver.py
+++ examples/wsgiserver.py
@@ -1,18 +1,17 @@
-"""This is a simple example of running a wsgi application with gevent.
-For a more fully-featured server which supports multiple processes,
-multiple threads, and graceful code reloading, see:
-
-http://pypi.python.org/pypi/Spawning/
+#/usr/bin/python
+"""Simple wsgi app. Serving on 8088.
 """
-from gevent import wsgi, socket
+
+from gevent import wsgi2
 
 def hello_world(env, start_response):
-    if env['PATH_INFO'] != '/':
-        start_response('404 Not Found', [('Content-Type', 'text/plain')])
-        return ['Not Found\r\n']
-    else:
+    if env['PATH_INFO'] == '/':
         start_response('200 OK', [('Content-Type', 'text/plain')])
         return ["Hello World!\r\n"]
+    else:
+        start_response('404 Not Found', [('Content-Type', 'text/plain')])
+        return ['Not Found\r\n']
 
-wsgi.server(socket.tcp_listener(('', 8080)), hello_world)
+print __doc__
+wsgi2.WSGIServer(('', 8088), hello_world).serve_forever()
 
