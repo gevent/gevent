2b8b548ab1c7ff618e7e252c6bc8c64508a1f62e
Denis Bilenko <denis.bilenko@gmail.com>
2009-09-09 17:23:37 +0700

    pool.Pool.join(): fix timeout bug: it used to raise Timeout instead of simply returning

--- gevent/pool.py
+++ gevent/pool.py
@@ -72,12 +72,16 @@ class GreenletSet(object):
 #         self.add = RaiseException("This %s has been closed" % self.__class__.__name__)
 
     def join(self, timeout=None, raise_error=False):
-        t = Timeout.start_new(timeout)
+        timeout = Timeout.start_new(timeout)
         try:
-            while self.greenlets:
-                joinall(self.greenlets, raise_error=raise_error)
+            try:
+                while self.greenlets:
+                    joinall(self.greenlets, raise_error=raise_error)
+            except Timeout, ex:
+                if ex is not timeout:
+                    raise
         finally:
-            t.cancel()
+            timeout.cancel()
 
     def kill(self, exception=GreenletExit, block=False, timeout=None):
         t = Timeout.start_new(timeout)
