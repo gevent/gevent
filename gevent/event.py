6ed3ea081cc5130e9bf7cd809e209b97bff2aab4
Denis Bilenko <denis.bilenko@gmail.com>
2009-09-28 13:11:22 +0700

    Event implementation is rewritten to match threading.Event more closely
    - Event.set() does not accept a value anymore; Event.get() is gone
    - "Event.set(); Event.clear()" now works as expected and does notify the
      waiters; it used to be a no-op with the old implementation
    - AsyncResult does not inherit from Event anymore but it inherited all
      the code of the old Event. It does not have clear() method anymore.
      Otherwise it works exactly like before.

--- gevent/event.py
+++ gevent/event.py
@@ -1,4 +1,9 @@
 # Copyright (c) 2009 Denis Bilenko. See LICENSE for details.
+"""Basic synchronization primitives: Event and AsyncResult.
+
+Event implements threading.Event interface but works across greenlets not threads.
+AsyncResult is a one-time Event that stores a result: value or exception.
+"""
 
 import sys
 import traceback
@@ -8,22 +13,127 @@ from gevent.timeout import Timeout, _NONE
 
 
 class Event(object):
+    """A synchronization primitive that allows one greenlet to wake up one or more others"""
 
     def __init__(self):
-        self._links = set()
-        self._value = _NONE
-        self._notifier = None
+        self._links = []
+        self._flag = False
+
+    def __str__(self):
+        return '<%s %s>' % (self.__class__.__name__, (self._flag and 'set') or 'clear')
 
     def is_set(self):
-        return self._value is not _NONE
+        return self._flag
 
-    isSet = is_set
-    ready = is_set
+    isSet = is_set # makes it a better drop-in replacement for threading.Event
+
+    def rawlink(self, callback):
+        if not callable(callback):
+            raise TypeError('Expected callable: %r' % (callback, ))
+        self._links.append(callback)
+        if self._flag:
+            core.active_event(self._notify_links, list(self._links))
+
+    def unlink(self, callback):
+        try:
+            self._links.remove(callback)
+        except ValueError:
+            pass
+
+    def clear(self):
+        """Clear the internal flag.
+        Subsequently, greenlets calling wait() will block until set() is called.
+        """
+        self._flag = False
+
+    def set(self):
+        self._flag = True
+        if self._links:
+            # schedule a job to notify the links already set
+            core.active_event(self._notify_links, list(self._links))
+
+    def _notify_links(self, links):
+        assert getcurrent() is get_hub()
+        for link in links:
+            if link in self._links: # check that link was not notified yet and was not removed by the client
+                try:
+                    link(self)
+                except:
+                    traceback.print_exc()
+                    try:
+                        sys.stderr.write('Failed to notify link %r of %r\n\n' % (link, self))
+                    except:
+                        pass
+
+    def wait(self, timeout=None):
+        if self._flag:
+            return
+        else:
+            switch = getcurrent().switch
+            self.rawlink(switch)
+            try:
+                t = Timeout.start_new(timeout)
+                try:
+                    result = get_hub().switch()
+                    assert result is self, 'Invalid switch into Event.wait(): %r' % (result, )
+                except Timeout, exc:
+                    if exc is not t:
+                        raise
+                finally:
+                    t.cancel()
+            finally:
+                self.unlink(switch)
+
+
+class AsyncResult(object):
+    """A one-time Event that stores a value or an exception.
+    
+    AsyncResult can be used to pass a value or exception to one or more waiters.
+    Its set() method accepts an argument - value to store. The sequential calls
+    to get() method will return the value passed:
+
+        >>> result = AsyncResult()
+        >>> result.set(100)
+        >>> result.get()
+        100
+
+    Additionally, it has set_exception() method, that causes get() to raise an error:
+
+        >>> result = AsyncResult()
+        >>> result.set_exception(RuntimeError('failure'))
+        >>> result.get()
+        Traceback (most recent call last):
+         ...
+        RuntimeError: failure
+
+    Similar to multiprocessing.AsyncResult, ready() and successful() methods are available.
+    Similar to Greenlet, AsyncResult has 'value' and 'exception' properties.
+    
+    AsyncResult instance can be used as link() target:
+
+        >>> result = AsyncResult()
+        >>> gevent.spawn(lambda : 1/0).link(result)
+        >>> result.get()
+        Traceback (most recent call last):
+         ...
+        ZeroDivisionError: integer division or modulo by zero
+    """
+    def __init__(self):
+        self._links = set()
+        self.value = None
+        self._exception = _NONE
+        self._notifier = None
+
+    def ready(self):
+        return self._exception is not _NONE
+
+    def successful(self):
+        return self._exception is None
 
     @property
-    def value(self):
-        if self._value is not _NONE:
-            return self._value
+    def exception(self):
+        if self._exception is not _NONE:
+            return self._exception
 
     def rawlink(self, callback):
         if not callable(callback):
@@ -35,26 +145,21 @@ class Event(object):
     def unlink(self, callback):
         self._links.discard(callback)
 
-    def clear(self):
-        """Reset the internal state. Subsequently, greenlets calling wait() or get()
-        will block until set() or put() is called.
-
-        Greenlets that called get() and wait() before clear() but were not yet notified
-        since the last put() call will _not_ be notified.
-        """
-        self._value = _NONE
-
     def set(self, value=None):
-        oldvalue = self._value
-        self._value = value
-        if oldvalue is _NONE:
-            if self._links and self._notifier is None:
-                self._notifier = core.active_event(self._notify_links)
+        self.value = value
+        self._exception = None
+        if self._links and self._notifier is None:
+            self._notifier = core.active_event(self._notify_links)
+
+    def set_exception(self, exception):
+        self._exception = exception
+        if self._links and self._notifier is None:
+            self._notifier = core.active_event(self._notify_links)
 
     def _notify_links(self):
         try:
             assert getcurrent() is get_hub()
-            while self._links and self._value is not _NONE:
+            while self._links:
                 link = self._links.pop()
                 try:
                     link(self)
@@ -64,13 +169,14 @@ class Event(object):
                         sys.stderr.write('Failed to notify link %r of %r\n\n' % (link, self))
                     except:
                         pass
-                # even if g is left unscheduled, it will be deallocated because there are no more references to it
         finally:
             self._notifier = None
 
     def get(self, block=True, timeout=None):
-        if self._value is not _NONE:
-            return self._value
+        if self._exception is not _NONE:
+            if self._exception is None:
+                return self.value
+            raise self._exception
         elif block:
             switch = getcurrent().switch
             self.rawlink(switch)
@@ -78,13 +184,15 @@ class Event(object):
                 t = Timeout.start_new(timeout)
                 try:
                     result = get_hub().switch()
-                    assert result is self, 'Invalid switch into Event.get(): %r' % (result, )
+                    assert result is self, 'Invalid switch into AsyncResult.get(): %r' % (result, )
                 finally:
                     t.cancel()
             except:
                 self.unlink(switch)
                 raise
-            return self._value
+            if self._exception is None:
+                return self.value
+            raise self._exception
         else:
             raise Timeout
 
@@ -92,7 +200,7 @@ class Event(object):
         return self.get(block=False)
 
     def wait(self, timeout=None):
-        if self._value is not _NONE:
+        if self._exception is not _NONE:
             return
         else:
             switch = getcurrent().switch
@@ -101,7 +209,7 @@ class Event(object):
                 t = Timeout.start_new(timeout)
                 try:
                     result = get_hub().switch()
-                    assert result is self, 'Invalid switch into Event.wait(): %r' % (result, )
+                    assert result is self, 'Invalid switch into AsyncResult.wait(): %r' % (result, )
                 finally:
                     t.cancel()
             except Timeout, exc:
@@ -114,74 +222,16 @@ class Event(object):
             # not calling unlink() in non-exception case, because if switch()
             # finished normally, link was already removed in _notify_links
 
-    def put(self, value=None):
-        import warnings
-        warnings.warn("Event.put is deprecated; use Event.set", DeprecationWarning, stacklevel=2)
-        return self.set(value=value)
-    
-
-class AsyncResult(Event):
-    """Like Greenlet, has 'value' and 'exception' properties, successful() method and get() can raise.
-
-    Unlike Event and Queue, AsyncResult instances can be used as link() targets:
-
-    >>> import gevent
-    >>> r = AsyncResult()
-    >>> gevent.spawn(lambda : 1/0).link(r)
-    >>> r.get()
-    Traceback (most recent call last):
-     ...
-    ZeroDivisionError: integer division or modulo by zero
-    """
-
-    @property
-    def value(self):
-        if self.ready():
-            result, exception = Event.get(self)
-            return result
-
-    @property
-    def exception(self):
-        if self.ready():
-            result, exception = Event.get(self)
-            return exception
-
-    def get(self, block=True, timeout=None):
-        result, exception = Event.get(self, block=block, timeout=timeout)
-        if exception is None:
-            return result
-        else:
-            raise exception
-
-    def successful(self):
-        result, exception = self._value
-        return exception is None
-
-    def set(self, value):
-        return Event.set(self, (value, None))
-
-    def set_exception(self, exception):
-        return Event.set(self, (None, exception))
-
+    # link protocol
     def __call__(self, source):
         if source.successful():
             self.set(source.value)
         else:
             self.set_exception(source.exception)
 
-    def put(self, value):
-        import warnings
-        warnings.warn("AsyncResult.put is deprecated; use AsyncResult.set", DeprecationWarning, stacklevel=2)
-        return self.set(value)
-
-    def put_exception(self, exception):
-        import warnings
-        warnings.warn("AsyncResult.put_exception is deprecated; use AsyncResult.set_exception", DeprecationWarning, stacklevel=2)
-        return self.set_exception(exception)
-    # QQQ add link_value and link_exception here?
-
 
 def waitall(events):
+    # QQQ add timeout?
     from gevent.queue import Queue
     queue = Queue()
     put = queue.put
