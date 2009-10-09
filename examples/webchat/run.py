cde41424bd8eae9e49007a55b55e60095f0cdeec
Denis Bilenko <denis.bilenko@gmail.com>
2009-10-09 14:39:31 +0700

    make examples/webchat/run.py work without PYTHONPATH

--- examples/webchat/run.py
+++ examples/webchat/run.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 from gevent import monkey; monkey.patch_all()
 from gevent.wsgi import WSGIServer
+import sys
 import os
 import traceback
 from django.core.handlers.wsgi import WSGIHandler
 from django.core.management import call_command
 from django.core.signals import got_request_exception
 
+sys.path.append('..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'webchat.settings'
 
 def exception_printer(sender, **kwargs):
