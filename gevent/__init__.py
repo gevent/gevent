8cad85706d94afb1a8b1f3009364b5c33b68a7a3
Denis Bilenko <denis.bilenko@gmail.com>
2009-11-15 15:36:32 +0600

    set version to 0.11.1

--- gevent/__init__.py
+++ gevent/__init__.py
@@ -4,8 +4,8 @@ gevent is a coroutine-based Python networking library that uses greenlet
 to provide a high-level synchronous API on top of libevent event loop.
 """
 
-version_info = (0, 11, 0)
-__version__ = '0.11.0'
+version_info = (0, 11, 1)
+__version__ = '0.11.1'
 
 __all__ = ['Greenlet',
            'GreenletExit',
