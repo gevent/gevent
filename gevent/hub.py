68490e3d431f295da27f5de9b3769aac3037501e
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-11 13:48:06 +0600

    Waiter: fix it to store value if no one waits for it
    - rename wait() to get() (wait is available as an alias for now)
    - add switch_args() helper that passes its arguments as a tuple to switch
    - __nonzero__() and waiting property are gone
    - ready() and successful() methods are added for completeness

--- gevent/hub.py
+++ gevent/hub.py
@@ -195,79 +195,110 @@ class DispatchExit(Exception):
 
 
 class Waiter(object):
-    """A low level synchronization class.
+    """A low level communication utility for greenlets.
 
-    Wrapper around greenlet's ``switch()`` and ``throw()`` calls that makes them safe:
+    Wrapper around greenlet's ``switch()`` and ``throw()`` calls that makes them somewhat safer:
 
-    * switching will occur only if the waiting greenlet is executing :meth:`wait`
-      method currently. Otherwise, :meth:`switch` and :meth:`throw` are no-ops.
+    * switching will occur only if the waiting greenlet is executing :meth:`get` method currently;
     * any error raised in the greenlet is handled inside :meth:`switch` and :meth:`throw`
+    * if :meth:`switch`/:meth:`throw` is called before the receiver calls :meth:`get`, then :class:`Waiter`
+      will store the value/exception. The following :meth:`get`will return the value/raise the exception.
 
     The :meth:`switch` and :meth:`throw` methods must only be called from the :class:`Hub` greenlet.
-    The :meth:`wait` method must be called from a greenlet other than :class:`Hub`.
+    The :meth:`get` method must be called from a greenlet other than :class:`Hub`.
+
+        >>> result = Waiter()
+        >>> _ = core.timer(0.1, result.switch, 'hello from Waiter')
+        >>> result.get() # blocks for 0.1 seconds
+        'hello from Waiter'
+
+    If switch is called before the greenlet gets a chance to call :meth:`get` then
+    :class:`Waiter` stores the value.
+
+        >>> result = Waiter()
+        >>> _ = core.timer(0.1, result.switch, 'hi from Waiter')
+        >>> sleep(0.2)
+        >>> result.get() # returns immediatelly without blocking
+        'hi from Waiter'
+
+    .. warning::
+
+        This a limited and dangerous way to communicate between greenlets. It can easily
+        left a greenlet unscheduled forever if used incorrectly. Consider using safer
+        :class:`Event`/:class:`AsyncResult`/:class:`Queue` classes.
     """
-    __slots__ = ['greenlet']
+
+    __slots__ = ['greenlet', 'value', '_exception']
 
     def __init__(self):
         self.greenlet = None
-
-    def __repr__(self):
-        if self.waiting:
-            waiting = ' waiting'
-        else:
-            waiting = ''
-        return '<%s at %s%s greenlet=%r>' % (type(self).__name__, hex(id(self)), waiting, self.greenlet)
+        self.value = None
+        self._exception = _NONE
 
     def __str__(self):
-        """
-        >>> print Waiter()
-        <Waiter greenlet=None>
-        """
-        if self.waiting:
-            waiting = ' waiting'
+        if self._exception is _NONE:
+            return '<%s greenlet=%s>' % (type(self).__name__, self.greenlet)
+        elif self._exception is None:
+            return '<%s greenlet=%s value=%r>' % (type(self).__name__, self.greenlet, self.value)
         else:
-            waiting = ''
-        return '<%s%s greenlet=%s>' % (type(self).__name__, waiting, self.greenlet)
+            return '<%s greenlet=%s exc_info=%r>' % (type(self).__name__, self.greenlet, self.exc_info)
 
-    def __nonzero__(self):
-        return self.greenlet is not None
+    def ready(self):
+        """Return true if and only if it holds a value or an exception"""
+        return self._exception is not _NONE
+
+    def successful(self):
+        """Return true if and only if it is ready and holds a value"""
+        return self._exception is None
 
     @property
-    def waiting(self):
-        return self.greenlet is not None
+    def exc_info(self):
+        "Holds the exception info passed to :meth:`throw` if :meth:`throw` was called. Otherwise ``None``."
+        if self._exception is not _NONE:
+            return self._exception
 
     def switch(self, value=None):
-        """Wake up the greenlet that is calling wait() currently (if there is one).
-        Can only be called from Hub's greenlet.
-        """
-        assert getcurrent() is get_hub(), "Can only use Waiter.switch method from the mainloop"
-        if self.greenlet is not None:
+        """Switch to the greenlet if one's available. Otherwise store the value."""
+        if self.greenlet is None:
+            self.value = value
+            self._exception = None
+        else:
+            assert getcurrent() is get_hub(), "Can only use Waiter.switch method from the Hub greenlet"
             try:
                 self.greenlet.switch(value)
             except:
                 traceback.print_exc()
 
+    def switch_args(self, *args):
+        return self.switch(args)
+
     def throw(self, *throw_args):
-        """Make greenlet calling wait() wake up (if there is a wait()).
-        Can only be called from Hub's greenlet.
-        """
-        assert getcurrent() is get_hub(), "Can only use Waiter.switch method from the mainloop"
-        if self.greenlet is not None:
+        """Switch to the greenlet with the exception. If there's no greenlet, store the exception."""
+        if self.greenlet is None:
+            self._exception = throw_args
+        else:
+            assert getcurrent() is get_hub(), "Can only use Waiter.switch method from the Hub greenlet"
             try:
                 self.greenlet.throw(*throw_args)
             except:
                 traceback.print_exc()
 
-    # XXX should be renamed to get() ? and the whole class is called Receiver?
-    def wait(self):
-        """Wait until switch() or throw() is called.
-        """
-        assert self.greenlet is None, 'This Waiter is already used by %r' % (self.greenlet, )
-        self.greenlet = getcurrent()
-        try:
-            return get_hub().switch()
-        finally:
-            self.greenlet = None
+    def get(self):
+        """If a value/an exception is stored, return/raise it. Otherwise until switch() or throw() is called."""
+        if self._exception is not _NONE:
+            if self._exception is None:
+                return self.value
+            else:
+                getcurrent().throw(*self._exception)
+        else:
+            assert self.greenlet is None, 'This Waiter is already used by %r' % (self.greenlet, )
+            self.greenlet = getcurrent()
+            try:
+                return get_hub().switch()
+            finally:
+                self.greenlet = None
+
+    wait = get # XXX backward compatibility; will be removed in the next release
 
     # can also have a debugging version, that wraps the value in a tuple (self, value) in switch()
     # and unwraps it in wait() thus checking that switch() was indeed called
