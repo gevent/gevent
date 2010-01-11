a29261425fb8f75624f9c33c98fcbd38ba130f3c
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-11 15:36:34 +0600

    test__socket_dns.py: add a bunch of new tests

--- greentest/test__socket_dns.py
+++ greentest/test__socket_dns.py
@@ -1,35 +1,148 @@
 #!/usr/bin/python
-from gevent import monkey; monkey.patch_all()
-from gevent import socket
+import sys
+import re
+import greentest
+import socket as real_socket
+from gevent.socket import *
 
-import unittest
 
-class GetAddrInfoTest(unittest.TestCase):
+ACCEPTED_GAIERROR_MISMATCH = {
+    "gaierror(-5, 'No address associated with hostname')": "DNSError(3, 'name does not exist')"
+}
+
+assert gaierror is real_socket.gaierror
+assert error is real_socket.error
+
+VERBOSE = '-v' in sys.argv
+
+
+class TestCase(greentest.TestCase):
+
+    def _test(self, hostname, check_ip=None):
+        self._test_gethostbyname(hostname, check_ip=check_ip)
+        self._test_getaddrinfo(hostname)
+
+    def _test_gethostbyname(self, hostname, check_ip=None):
+        try:
+            real_ip = real_socket.gethostbyname(hostname)
+        except Exception, ex:
+            real_ip = ex
+        try:
+            ip = gethostbyname(hostname)
+        except Exception, ex:
+            ip = ex
+        if self.equal(real_ip, ip):
+            return ip
+        self.assertEqual(real_ip, ip)
+        if check_ip is not None:
+            self.assertEqual(check_ip, ip)
+        return ip
+
+    PORTS = [80, 0, 53]
+    getaddrinfo_args = [(),
+                        (AF_UNSPEC, ),
+                        (AF_INET, SOCK_STREAM, ),
+                        (AF_UNSPEC, SOCK_DGRAM, ),
+                        (AF_INET, SOCK_RAW, ),
+                        (AF_UNSPEC, SOCK_STREAM, 6),
+                        (AF_INET, SOCK_DGRAM, 17)]
+
+
+    def _test_getaddrinfo(self, hostname):
+
+        for port in self.PORTS:
+            for args in self.getaddrinfo_args:
+                try:
+                    real_ip = real_socket.getaddrinfo(hostname, port, *args)
+                except Exception, ex:
+                    real_ip = ex
+                try:
+                    ip = getaddrinfo(hostname, port, *args)
+                except Exception, ex:
+                    ip = ex
+                if not self.equal(real_ip, ip):
+                    args_str = ', '.join(repr(x) for x in (hostname, port) + args)
+                    print 'WARNING: getaddrinfo(%s):\n    %r\n != %r' % (args_str, real_ip, ip)
+        # QQQ switch_expected becomes useless when a bunch of unrelated tests are merged
+        #     into a single one like above. Generate individual test cases instead?
+
+    def equal(self, a, b):
+        if a == b:
+            return True
+        if isinstance(a, Exception) and isinstance(b, Exception):
+            if repr(a) == repr(b):
+                return True
+            if ACCEPTED_GAIERROR_MISMATCH.get(repr(a), repr(b))==repr(b):
+                return True
+
+    def checkEqual(self, a, b):
+        if a == b:
+            return
+        print 'WARNING: %s.%s:\n    %r\n != %r' % (self.__class__.__name__, self.testname, a, b)
+
+
+def get_test(ip, host):
+
+    def test(self):
+        self._test(host, check_ip=ip)
+    test.__name__ = 'test_' + re.sub('[^\w]', '_', host + '__' + ip)
+
+    return test
+
+
+class TestLocal(TestCase):
+
+    switch_expected = False
+
+    def test_hostname(self):
+        hostname = real_socket.gethostname()
+        self._test(hostname)
 
     def test_localhost(self):
-        socket.getaddrinfo('localhost', 0)
-        socket.getaddrinfo('127.0.0.1', 0)
+        self._test('localhost')
 
-    def test_google(self):
-        socket.getaddrinfo('www.google.com', 80)
+    def test_127_0_0_1(self):
+        self._test('127.0.0.1')
 
-class GetNameInfoTest(unittest.TestCase):
+    def test_1_2_3_4(self):
+        self._test('1.2.3.4')
 
-    def test_localhost(self):
-        socket.getnameinfo(('127.0.0.1', 0), 0)
+    def test_notexistent(self):
+        # not really interesting because the original gethostbyname() is called for everything without dots
+        self._test('notexistent')
 
-    def test_google(self):
-        socket.getnameinfo(('74.125.19.105', 80), 0)
+    def test_None(self):
+        self._test(None)
 
-class GetHostByNameTest(unittest.TestCase):
+    def test_25(self):
+        self._test(25)
 
-    def test_localhost(self):
-        socket.gethostbyname('localhost')
-        socket.gethostbyname('127.0.0.1')
+    try:
+        etc_hosts = open('/etc/hosts').read()
+    except IOError:
+        data = ''
 
-    def test_google(self):
-        socket.gethostbyname('www.google.com')
+    for ip, host in re.findall(r'^\s*(\d+\.\d+\.\d+\.\d+)\s+([^\s]+)', etc_hosts, re.M)[:10]:
+        func = get_test(ip, host)
+        print 'Adding %s' % func.__name__
+        locals()[func.__name__] = func
+        del func
 
-if __name__ == "__main__":
-    unittest.main()
+
+class TestRemote(TestCase):
+
+    switch_expected = True
+
+    def test_www_python_org(self):
+        self._test('www.python.org')
+
+    def test_notexistent_tld(self):
+        self._test('myhost.mytld')
+
+    def test_notexistent_dot_com(self):
+        self._test('sdfsdfgu5e66098032453245wfdggd.com')
+
+
+if __name__ == '__main__':
+    greentest.main()
 
