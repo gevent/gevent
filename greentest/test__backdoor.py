4704731f7e666dca213ede48b7d5d4f92ac757ed
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-28 14:23:04 +0600

    update test__backdoor.py

--- greentest/test__backdoor.py
+++ greentest/test__backdoor.py
@@ -3,21 +3,22 @@ import gevent
 from gevent import socket
 from gevent import backdoor
 
+
 class Test(greentest.TestCase):
 
     def test(self):
-        g = gevent.spawn(backdoor.backdoor_server, socket.tcp_listener(('127.0.0.1', 7891)))
+        server = backdoor.BackdoorServer.spawn(('127.0.0.1', 7891))
         gevent.sleep(0.1)
-        s = socket.create_connection(('127.0.0.1', 7891))
-        f = s.makefile()
+        fileobj = socket.create_connection(('127.0.0.1', 7891)).makefile()
         while True:
-            line = gevent.with_timeout(0.1, f.readline, timeout_value=None)
+            line = gevent.with_timeout(0.1, fileobj.readline, timeout_value=None)
             if line is None:
                 break
-        s.sendall('2+2\r\n')
-        l = f.readline()
-        assert l.strip() == '4', repr(l)
-        g.kill(block=True)
+        fileobj.write('2+2\r\n')
+        fileobj.flush()
+        line = fileobj.readline()
+        assert line.strip() == '4', repr(line)
+        server.kill(block=True)
 
 
 if __name__ == '__main__':
