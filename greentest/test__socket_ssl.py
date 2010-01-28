0d00390bac92ad509776000fdce2b436ed2b8614
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-28 15:51:36 +0600

    test__socket_ssl.py: add tests for __str__ and __repr__ of socket objects

--- greentest/test__socket_ssl.py
+++ greentest/test__socket_ssl.py
@@ -3,6 +3,7 @@ from gevent import monkey; monkey.patch_all()
 
 import unittest
 import httplib
+import socket
 
 class AmazonHTTPSTests(unittest.TestCase):
 
@@ -11,6 +12,14 @@ class AmazonHTTPSTests(unittest.TestCase):
         conn.debuglevel = 1
         conn.request('GET', '/')
         resp = conn.getresponse()
+
+    def test_str_and_repr(self):
+        conn = socket.socket()
+        conn.connect(('sdb.amazonaws.com', 443))
+        ssl_conn = socket.ssl(conn)
+        assert str(ssl_conn)
+        assert repr(ssl_conn)
+
 
 if __name__ == "__main__":
     unittest.main()
