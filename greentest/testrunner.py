0d178edf295a6833de0537e6e880d0687b34a6a6
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-29 16:57:01 +0600

    set default timeout for tests to 60 seconds

--- greentest/testrunner.py
+++ greentest/testrunner.py
@@ -36,7 +36,7 @@ one will be selected if not provided.
 # - screws up warnings location, causing them to appear as originated from testrunner.py
 
 # the number of seconds each test script is allowed to run
-DEFAULT_TIMEOUT = 20
+DEFAULT_TIMEOUT = 60
 
 # the number of bytes of output that is recorded; the rest is thrown away
 OUTPUT_LIMIT = 100*1024
