897c7cd365da87a5430ba2e3a4d6cbc01d19711e
Denis Bilenko <denis.bilenko@gmail.com>
2010-01-29 17:25:09 +0600

    add prepare_stdlib_test() to greentest

--- greentest/greentest.py
+++ greentest/greentest.py
@@ -180,3 +180,36 @@ def patch_all(timeout=None):
     if timeout is not None:
         TestCase.__timeout__ = timeout
 
+
+
+class ContainsAll(object):
+    def __contains__(self, item):
+        return True
+
+
+def prepare_stdlib_test(filename):
+    from test import test_support
+    test_support.use_resources = ContainsAll()
+
+    name = filename.replace('_patched', '').replace('.py', '')
+
+    os.environ['__module_name__'] = name
+
+    try:
+        # XXX importing just to find where it resides; need a function that just returns the path
+        package = __import__('test.%s' % name)
+    except:
+        print >> sys.stderr, 'ModuleNotFoundWarning: cannot import test.%s' % name
+        sys.exit(0)
+
+    module = getattr(package, name)
+    _filename = module.__file__.replace('.pyc', '.py')
+
+    module_source = open(_filename).read()
+    from patched_tests_setup import disable_tests_in_the_source
+    module_source = disable_tests_in_the_source(module_source, name)
+    module_code = compile(module_source, _filename, 'exec')
+
+    print >> sys.stderr, 'Testing %s with monkey patching' % _filename
+    patch_all(timeout=20)
+    return module_code
