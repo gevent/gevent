78c78d2645b8d2fe70317a727e96b1e74e30c60f
Denis Bilenko <denis.bilenko@gmail.com>
2010-02-22 10:44:45 +0600

    update copyright years

--- gevent/coros.py
+++ gevent/coros.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2008-2009, AG Projects
-# Copyright (c) 2009, Denis Bilenko
+# Copyright (c) 2009-2010, Denis Bilenko
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
