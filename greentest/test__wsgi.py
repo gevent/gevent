13c10ba6bbc168617c2bbb56039ee65fb55d0c97
Denis Bilenko <denis.bilenko@gmail.com>
2010-04-07 19:33:06 +0700

    fix test__wsgi.py to work with the new wsgi_test.py

--- greentest/test__wsgi.py
+++ greentest/test__wsgi.py
@@ -1,7 +1,10 @@
 from gevent import wsgi
+import wsgi_test
 from wsgi_test import *
 
-del TestHttps, TestChunkedApp, TestBigChunks
+del TestHttps
+
+wsgi_test.server_implements_chunked = False
 
 TestCase.get_wsgi_module = lambda *args: wsgi
 
