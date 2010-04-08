d4e48655a766c4b6c9bfcf6364e1332cfee50496
Denis Bilenko <denis.bilenko@gmail.com>
2010-04-08 21:46:57 +0700

    rename ss to server; add a test where close() method is called

--- greentest/test_server.py
+++ greentest/test_server.py
@@ -9,30 +9,33 @@ class TestFatalErrors(greentest.TestCase):
 
     def setUp(self):
         greentest.TestCase.setUp(self)
-        self.ss = server.StreamServer(("127.0.0.1", 0))
-        self.ss.pre_start()
-        self.socket = self.ss.socket
-        self.ss.start()
+        self.server = server.StreamServer(("127.0.0.1", 0))
+        self.server.start()
+        self.socket = self.server.socket
 
-    def _joinss(self):
-        self.ss.join(0.1)
+    def _join_server(self):
+        self.server.join(0.1)
         try:
-            self.assert_(self.ss.ready(), "server did not die")
+            self.assert_(self.server.ready(), "server did not die")
         finally:
-            self.ss.kill()
+            self.server.kill(block=True)
 
     def test_socket_shutdown(self):
         self.socket.shutdown(socket.SHUT_RDWR)
-        self._joinss()
+        self._join_server()
 
     def test_socket_close(self):
+        self.socket.close()
+        self._join_server()
+
+    def test_socket_close_fileno(self):
         os.close(self.socket.fileno())
-        self._joinss()
+        self._join_server()
 
     def test_socket_file(self):
         os.close(self.socket.fileno())
         f = open("/dev/zero", "r")
-        self._joinss()
+        self._join_server()
         del f
 
 
