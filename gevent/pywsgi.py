f0c7c167e1412e9b7d11bb808a742c6fc4663027
Denis Bilenko <denis.bilenko@gmail.com>
2010-04-08 13:50:47 +0700

    add a copyright notice

--- gevent/pywsgi.py
+++ gevent/pywsgi.py
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2005-2006, Bob Ippolito
 # Copyright (c) 2007, Linden Research, Inc.
+# Copyright (c) 2009-2010, gevent contributors
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
