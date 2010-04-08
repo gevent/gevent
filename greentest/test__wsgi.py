21e9911444710425f6f12216385f4fe5576e7f8f
Denis Bilenko <denis.bilenko@gmail.com>
2010-04-08 16:37:27 +0700

    add test that gevent.wsgi raises a TypeError when passed SSLObject

--- greentest/test__wsgi.py
+++ greentest/test__wsgi.py
@@ -7,5 +7,18 @@ wsgi_test.server_implements_chunked = False
 TestCase.get_wsgi_module = lambda *args: wsgi
 
 
+class TestHttpsError(HttpsTestCase):
+
+    def setUp(self):
+        listener = self.get_listener()
+        self.assertRaises(TypeError, self.get_wsgi_module().WSGIServer, listener, self.application)
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
+
+  
 if __name__ == '__main__':
     greentest.main()
