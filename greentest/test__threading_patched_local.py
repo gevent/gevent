24685bcfd5f3fa2c0c8ba58fe3aab344c0fcfac9
Denis Bilenko <denis.bilenko@gmail.com>
2010-05-15 22:39:28 +0700

    add a test case exposing issue #24. Thanks to Ted Suzman.
    http://code.google.com/p/gevent/issues/detail?id=24

---
+++ greentest/test__threading_patched_local.py
@@ -0,0 +1,21 @@
+from gevent import monkey; monkey.patch_all()
+import threading
+
+localdata = threading.local()
+localdata.x = "hello"
+
+success = []
+
+def func():
+    try:
+        localdata.x
+        raise AssertionError('localdata.x must raise AttributeError')
+    except AttributeError:
+        pass
+    assert localdata.__dict__ == {}, localdata.__dict__
+    success.append(1)
+
+t = threading.Thread(None, func)
+t.start()
+t.join()
+assert success == [1], 'test failed'
