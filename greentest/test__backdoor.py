a49eca429c2c87de2d7cc78bde166c45ea928657
Denis Bilenko <denis.bilenko@gmail.com>
2010-06-06 23:43:33 +0700

    fix test__backdoor.py to work on freebsd

--- greentest/test__backdoor.py
+++ greentest/test__backdoor.py
@@ -4,19 +4,25 @@ from gevent import socket
 from gevent import backdoor
 
 
+def read_until(conn, postfix):
+    read = ''
+    while not read.endswith(postfix):
+        result = conn.recv(1)
+        if not result:
+            raise AssertionError('Connection ended before %r. Data read:\n%r' % (postfix, read))
+        read += result
+    return read
+
+
 class Test(greentest.TestCase):
 
     def test(self):
         server = backdoor.BackdoorServer.spawn(('127.0.0.1', 7891))
-        gevent.sleep(0.1)
-        fileobj = socket.create_connection(('127.0.0.1', 7891)).makefile()
-        while True:
-            line = gevent.with_timeout(0.1, fileobj.readline, timeout_value=None)
-            if line is None:
-                break
-        fileobj.write('2+2\r\n')
-        fileobj.flush()
-        line = fileobj.readline()
+        gevent.sleep(0)
+        conn = socket.create_connection(('127.0.0.1', 7891))
+        read_until(conn, '>>> ')
+        conn.sendall('2+2\r\n')
+        line = conn.makefile().readline()
         assert line.strip() == '4', repr(line)
         server.kill(block=True)
 
