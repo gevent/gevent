908c4adf457f17bc153bd0b4a3240ab9b1636837
Denis Bilenko <denis.bilenko@gmail.com>
2010-06-09 13:03:50 +0700

    test__wsgi.py: remove TestHttpsError, it is covered by test__server.TestSSLSocketNotAllowed

--- greentest/test__wsgi.py
+++ greentest/test__wsgi.py
@@ -9,22 +9,5 @@ test__pywsgi.server_supports_pipeline = gevent.core.get_version()[1] == '2'
 TestCase.get_wsgi_module = lambda *args: wsgi
 
 
-class TestHttpsError(HttpsTestCase):
-
-    def setUp(self):
-        from gevent.socket import ssl, socket
-        listener = socket()
-        listener.bind(('0.0.0.0', 0))
-        listener.listen(5)
-        listener = ssl(listener, keyfile=self.keyfile, certfile=self.certfile)
-        self.assertRaises(TypeError, self.get_wsgi_module().WSGIServer, listener, self.application)
-
-    def tearDown(self):
-        pass
-
-    def test(self):
-        pass
-
-  
 if __name__ == '__main__':
     greentest.main()
