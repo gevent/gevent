99dc6b95e17f9a377b4b28c6475c6902e5884201
Denis Bilenko <denis.bilenko@gmail.com>
2010-07-01 13:36:44 +0700

    rename '_timer' to 'timer' in internal class select.SelectResult

--- gevent/select.py
+++ gevent/select.py
@@ -27,17 +27,17 @@ class SelectResult(object):
         self.read = []
         self.write = []
         self.event = Event()
-        self._timer = None
+        self.timer = None
 
     def update(self, event, evtype):
         if evtype & core.EV_READ:
             self.read.append(event.arg)
-            if self._timer is None:
-                self._timer = core.timer(0, self.event.set)
+            if self.timer is None:
+                self.timer = core.timer(0, self.event.set)
         elif evtype & core.EV_WRITE:
             self.write.append(event.arg)
-            if self._timer is None:
-                self._timer = core.timer(0, self.event.set)
+            if self.timer is None:
+                self.timer = core.timer(0, self.event.set)
         # using core.timer(0, ...) to let other active events call update() before Event.wait() returns
 
 
