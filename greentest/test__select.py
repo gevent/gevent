58045ce638d2ab09e035ae4050b34675aa1f0ab5
Denis Bilenko <denis.bilenko@gmail.com>
2010-08-04 15:03:54 +0700

    add test for select([1L], ...)

--- greentest/test__select.py
+++ greentest/test__select.py
@@ -9,5 +9,12 @@ class TestSelect(greentest.GenericWaitTestCase):
         select.select([], [], [], timeout)
 
 
+class TestSelectTypes(greentest.TestCase):
+
+    def test(self):
+        select.select([1], [], [], 0.001)
+        select.select([1L], [], [], 0.001)
+
+
 if __name__=='__main__':
     greentest.main()
