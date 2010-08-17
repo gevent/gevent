42a9ae9cbbae2e4f003d321b4c1aa3af145eff1f
Denis Bilenko <denis.bilenko@gmail.com>
2010-08-17 16:45:10 +0700

    test for issue #39

---
+++ greentest/test__semaphore.py
@@ -0,0 +1,14 @@
+import greentest
+from gevent.coros import Semaphore
+
+class TestTimeoutAcquire(greentest.TestCase):
+
+    # issue 39
+    def test_acquire_returns_false_after_timeout(self):
+        s = Semaphore(value=0)
+        result = s.acquire(timeout=0.01)
+        assert result is False, repr(result)
+
+
+if __name__=='__main__':
+    greentest.main()
