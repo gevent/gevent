4cb3131209d2ced316b7425ce6662c0f36a4ef88
Denis Bilenko <denis.bilenko@gmail.com>
2010-09-15 15:44:56 +0700

    Python Issue #5238: Calling makefile() on an SSL object would prevent the underlying socket from being closed until all objects get truely destroyed. Patch by Antoine Pitrou.
    from http://svn.python.org/view?view=rev&revision=80428

--- gevent/ssl.py
+++ gevent/ssl.py
@@ -367,7 +367,9 @@ class SSLSocket(socket):
         works with the SSL connection.  Just use the code
         from the socket module."""
         self._makefile_refs += 1
-        return _fileobject(self, mode, bufsize)
+        # close=True so as to decrement the reference count when done with
+        # the file-like object.
+        return _fileobject(self, mode, bufsize, close=True)
 
 
 def wrap_socket(sock, keyfile=None, certfile=None,
