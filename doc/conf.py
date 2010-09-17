046525b23ef556688cdc325b7d7f1285725a1027
Denis Bilenko <denis.bilenko@gmail.com>
2010-09-17 18:36:37 +0700

    conf.py: set add_function_parentheses to True explicitly

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ exclude_trees = ['_build']
 #default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
