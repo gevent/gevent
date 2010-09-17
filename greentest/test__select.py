c26eac337e18a6454ee3af2bb9dccf9c55dc3b72
Denis Bilenko <denis.bilenko@gmail.com>
2010-09-17 17:45:03 +0700

    test__select.py split TestSelectTypes.test into test_int and test_long and add test_string

--- greentest/test__select.py
+++ greentest/test__select.py
@@ -11,9 +11,15 @@ class TestSelect(greentest.GenericWaitTestCase):
 
 class TestSelectTypes(greentest.TestCase):
 
-    def test(self):
+    def test_int(self):
         select.select([1], [], [], 0.001)
+
+    def test_long(self):
         select.select([1L], [], [], 0.001)
+
+    def test_string(self):
+        self.switch_expected = False
+        self.assertRaises(TypeError, select.select, ['hello'], [], [], 0.001)
 
 
 if __name__ == '__main__':
