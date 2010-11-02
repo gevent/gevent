e2b7c78408740da46bf100f2ee13b915dee1f472
Denis Bilenko <denis.bilenko@gmail.com>
2010-11-02 16:59:22 +0600

    test__socket_close.py: create socket inside the test case (to pass totalrefcount check)
    --HG--
    extra : transplant_source : Y%E7D%FB%91%DE%AD0%FFQEZ%C0%12%7C%C4A%E7%B5%C2

--- greentest/test__socket_close.py
+++ greentest/test__socket_close.py
@@ -7,26 +7,23 @@ import greentest
 
 class Test(greentest.TestCase):
 
-    def setUp(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('www.google.com', 80))
-
-    def tearDown(self):
-        self.sock.close()
-
     def test_recv_closed(self):
-        receiver = gevent.spawn(self.sock.recv, 25)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('www.google.com', 80))
+        receiver = gevent.spawn(sock.recv, 25)
         gevent.sleep(0.001)
-        self.sock.close()
+        sock.close()
         receiver.join(timeout=0.001)
         assert receiver.ready(), receiver
         self.assertEqual(receiver.value, '')
 
     def test_recv_twice(self):
-        receiver = gevent.spawn(self.sock.recv, 25)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('www.google.com', 80))
+        receiver = gevent.spawn(sock.recv, 25)
         gevent.sleep(0.001)
-        self.assertRaises(AssertionError, self.sock.recv, 25)
-        self.assertRaises(AssertionError, self.sock.recv, 25)
+        self.assertRaises(AssertionError, sock.recv, 25)
+        self.assertRaises(AssertionError, sock.recv, 25)
         receiver.kill()
 
 
