6b8261315f397702e2ce7e49cb893edc69cd6218
Denis Bilenko <denis.bilenko@gmail.com>
2011-01-27 13:11:21 +0600

    test__issue58.py: support python 2.5
    --HG--
    extra : transplant_source : I8%8F%CF%9C%8D%84%B1%D9%29%A4E%8C%3DHf%DD%F6%9E%E8

--- greentest/test__issue58.py
+++ greentest/test__issue58.py
@@ -15,5 +15,8 @@ server = wsgi.WSGIServer(('', 0), error)
 server.start()
 conn = socket.create_connection(('127.0.0.1', server.server_port))
 conn.sendall('GET / HTTP/1.1\r\nConnection: close\r\n\r\n')
-with gevent.Timeout(0.1):
+timeout = gevent.Timeout.start_new(0.1)
+try:
     conn.makefile(bufsize=1).read()
+finally:
+    timeout.cancel()
