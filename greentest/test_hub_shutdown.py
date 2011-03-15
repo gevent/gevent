c01cefa3ae1912d1eedc8ee5b8843ac91d8edb14
Denis Bilenko <denis.bilenko@gmail.com>
2011-03-15 17:34:04 +0600

    fix test_hub_shutdown.py

--- greentest/test_hub_shutdown.py
+++ greentest/test_hub_shutdown.py
@@ -1,5 +1,6 @@
-"""Tests that Hub.shutdown() works even when hub is not yet started"""
+"""Tests that Hub.join() works even when hub is not yet started"""
+# rename to test_hub_join()?
 import gevent.hub
-res = gevent.hub.get_hub().shutdown()
+res = gevent.hub.get_hub().join()
 assert res is None, res
 assert 'hub' not in gevent.hub._threadlocal.__dict__, gevent.hub._threadlocal.__dict__
