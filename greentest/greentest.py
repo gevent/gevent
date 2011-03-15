4113476b57e4e53c6d4593fd1d5c7f732e8e30b0
Denis Bilenko <denis.bilenko@gmail.com>
2011-03-15 17:21:12 +0600

    increase delay, just in case

--- greentest/greentest.py
+++ greentest/greentest.py
@@ -282,10 +282,10 @@ class GenericWaitTestCase(TestCase):
 
     def test_returns_none_after_timeout(self):
         start = time.time()
-        result = self.wait(timeout=0.01)
+        result = self.wait(timeout=0.02)
         # join and wait simply returns after timeout expires
         delay = time.time() - start
-        assert 0.01 - 0.001 <= delay < 0.01 + 0.01, delay
+        assert 0.02 - 0.002 <= delay < 0.02 + 0.02, delay
         assert result is None, repr(result)
 
 
