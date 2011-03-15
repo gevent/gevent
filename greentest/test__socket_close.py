514852f601239ade950bd27b8b66bed8f99c3842
Denis Bilenko <denis.bilenko@gmail.com>
2011-03-15 17:32:39 +0600

    test__socket_close.py: use local server

--- greentest/test__socket_close.py
+++ greentest/test__socket_close.py
@@ -1,25 +1,38 @@
 import gevent
 from gevent import socket
+from gevent import server
 import greentest
 
 # XXX also test: send, sendall, recvfrom, recvfrom_into, sendto
 
 
+def readall(socket, address):
+    while socket.recv(1024):
+        pass
+
+
 class Test(greentest.TestCase):
+
+    def setUp(self):
+        self.server = server.StreamServer(('', 0), readall)
+        self.server.start()
+
+    def tearDown(self):
+        self.server.stop()
 
     def test_recv_closed(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('www.google.com', 80))
+        sock.connect(('127.0.0.1', self.server.server_port))
         receiver = gevent.spawn(sock.recv, 25)
         gevent.sleep(0.001)
         sock.close()
-        receiver.join(timeout=0.001)
+        receiver.join(timeout=0.1)
         assert receiver.ready(), receiver
         self.assertEqual(receiver.value, '')
 
     def test_recv_twice(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('www.google.com', 80))
+        sock.connect(('127.0.0.1', self.server.server_port))
         receiver = gevent.spawn(sock.recv, 25)
         gevent.sleep(0.001)
         self.assertRaises(AssertionError, sock.recv, 25)
