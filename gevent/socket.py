ab5b3850f3b325cd23b6e50ff9f5ab1b107d4e11
Denis Bilenko <denis.bilenko@gmail.com>
2011-04-07 15:21:48 +0700

    socket.py: rename an argument of _wait() from 'io' to 'watcher'

--- gevent/socket.py
+++ gevent/socket.py
@@ -301,27 +301,25 @@ class socket(object):
             result += ' timeout=' + str(self.timeout)
         return result
 
-    def _wait(self, io, timeout_exc=timeout('timed out')):
-        """Block the current greenlet until *io* is ready.
+    def _wait(self, watcher, timeout_exc=timeout('timed out')):
+        """Block the current greenlet until *watcher* has pending events.
 
         If *timeout* is non-negative, then *timeout_exc* is raised after *timeout* second has passed.
         By default *timeout_exc* is ``socket.timeout('timed out')``.
 
         If :func:`cancel_wait` is called, raise ``socket.error(EBADF, 'File descriptor was closed in another greenlet')``.
         """
-        assert io.callback is None, 'This socket is already used by another greenlet: %r' % (io.callback, )
+        assert watcher.callback is None, 'This socket is already used by another greenlet: %r' % (watcher.callback, )
         if self.timeout is not None:
             timeout = Timeout.start_new(self.timeout, timeout_exc)
         else:
             timeout = None
         try:
-            self.hub.wait(io)
+            self.hub.wait(watcher)
         finally:
             if timeout is not None:
                 timeout.cancel()
 
-    # rename "io" to "watcher" because wait() works with any watcher
-    # make wait() a Hub's method
     def accept(self):
         sock = self._sock
         while True:
