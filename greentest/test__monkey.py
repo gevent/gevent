61b3e4b76df0cd140859d8b2f977766924e597fd
Denis Bilenko <denis.bilenko@gmail.com>
2011-08-29 15:50:59 +0700

    test__monkey.py: check if 'create_connection' was replaced

--- greentest/test__monkey.py
+++ greentest/test__monkey.py
@@ -9,3 +9,7 @@ import threading
 assert 'built-in' not in repr(thread.start_new_thread), repr(thread.start_new_thread)
 assert 'built-in' not in repr(threading._start_new_thread), repr(threading._start_new_thread)
 assert 'built-in' not in repr(threading._sleep), repr(threading._sleep)
+
+import socket
+from gevent import socket as gevent_socket
+assert socket.create_connection is gevent_socket.create_connection
