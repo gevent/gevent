605ca0be907ca051795fa5d2e9ad117585244327
Ralf Schmitt <ralf@systemexit.de>
2011-10-28 23:51:46 +0200

    dynamically generate test functions in test__execmodules.py

--- greentest/test__execmodules.py
+++ greentest/test__execmodules.py
@@ -1,11 +1,24 @@
 import sys
-import traceback
-from greentest import walk_modules
+from greentest import walk_modules, BaseTestCase, main
+
+
+class TestExec(BaseTestCase):
+    pass
+
+
+def make_exec_test(path, module):
+
+    def test(self):
+        sys.stderr.write('%s %s\n' % (module, path))
+        execfile(path, {})
+
+    name = "test_" + module.replace(".", "_")
+    test.__name__ = name
+    setattr(TestExec, name, test)
 
 
 for path, module in walk_modules():
-    sys.stderr.write('%s %s\n' % (module, path))
-    try:
-        execfile(path)
-    except Exception:
-        traceback.print_exc()
+    make_exec_test(path, module)
+
+if __name__ == '__main__':
+    main()
