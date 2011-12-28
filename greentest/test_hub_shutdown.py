3a7dc2df75255ee7bbdc0b2bdda11bba3a2a80e0
Denis Bilenko <denis.bilenko@gmail.com>
2011-12-28 13:44:52 +0600

    rename test_hub_shutdown.py to test_hub_join() and extend it a bit
    --HG--
    rename : greentest/test_hub_shutdown.py => greentest/test_hub_join.py

--- greentest/test_hub_shutdown.py
+++
@@ -1,7 +0,0 @@
-"""Tests that Hub.join() works even when hub is not yet started"""
-# rename to test_hub_join()?
-import gevent.hub
-res = gevent.hub.get_hub().join()
-assert res is True, res
-res = gevent.hub.get_hub().join()
-assert res is True, res
