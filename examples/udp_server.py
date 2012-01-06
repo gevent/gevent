9c320f071d882dca52cfa8237cfca1b9e2ef55f4
Denis Bilenko <denis.bilenko@gmail.com>
2012-01-07 00:09:30 +0700

    add examples/udp_server.py

---
+++ examples/udp_server.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2012 Denis Bilenko. See LICENSE for details.
+"""A simple UDP server.
+
+For every message received, it sends a reply back.
+
+You can use udp_client.py to send a message.
+"""
+from gevent.server import DatagramServer
+
+
+class EchoServer(DatagramServer):
+
+    def handle(self, data, address):
+        print '%s: got %r' % (address[0], data)
+        self.socket.sendto('Received %s bytes' % len(data), address)
+
+
+if __name__ == '__main__':
+    EchoServer(':9000').serve_forever()
