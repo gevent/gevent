997e5248385d26b446bbcbfed0d1ccdd8fcad490
Denis Bilenko <denis.bilenko@gmail.com>
2012-01-10 02:48:51 +0700

    add examples/threadpool.py

---
+++ examples/threadpool.py
@@ -0,0 +1,12 @@
+import time
+import gevent
+from gevent.threadpool import ThreadPool
+
+
+pool = ThreadPool(3)
+start = time.time()
+for _ in xrange(4):
+    pool.spawn(time.sleep, 1)
+gevent.run()
+delay = time.time() - start
+print 'Running "time.sleep(1)" 4 times with 3 threads. Should take about 2 seconds: %.3fs' % delay
