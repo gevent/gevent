099091c6da3f2115ebc179f2b7913107483bc45b
Denis Bilenko <denis.bilenko@gmail.com>
2012-01-10 20:30:00 +0700

    cythonpp.py: copy implementations of itertools.combinations() and itertools.product(). This fixes cythonpp.py on python 2.5.

--- util/cythonpp.py
+++ util/cythonpp.py
@@ -5,7 +5,6 @@ import os
 import re
 import traceback
 import datetime
-import itertools
 import pipes
 import difflib
 from hashlib import md5
@@ -37,13 +36,6 @@ def match_condition(line):
     return condition_re.match(line)
 
 newline_token = ' <cythonpp.py: REPLACE WITH NEWLINE!> '
-
-
-def combinations(p, r):
-    # python 2.6.1 on snow leopard fails with a ValueError on this case
-    if len(p) < r:
-        return []
-    return itertools.combinations(p, r)
 
 
 def process_filename(filename, output_filename=None):
@@ -654,7 +646,7 @@ def flat_tuple(x):
 
 
 def get_selections(items):
-    return set([flat_tuple(sorted(set(x))) for x in itertools.product(items, repeat=len(items))])
+    return set([flat_tuple(sorted(set(x))) for x in product(items, repeat=len(items))])
 
 
 def is_impossible(configuration):
@@ -720,6 +712,41 @@ def dbg(*args):
     if not DEBUG:
         return
     return log(*args)
+
+
+# itertools is only available in python 2.6
+# itertools.combinations has a problem on 2.6.1
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+
+
+def product(*args, **kwds):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = map(tuple, args) * kwds.get('repeat', 1)
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
 
 
 if __name__ == '__main__':
