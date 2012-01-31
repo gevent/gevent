1ffa8e05fdb97d9dff5c68dea5521f8e23e13fba
Denis Bilenko <denis.bilenko@gmail.com>
2012-01-31 20:04:46 +0700

    resolver_thread: do not print stack traces for exceptions

--- gevent/resolver_thread.py
+++ gevent/resolver_thread.py
@@ -8,6 +8,8 @@ __all__ = ['Resolver']
 
 class Resolver(object):
 
+    expected_errors = (_socket.error, TypeError)
+
     def __init__(self, hub=None):
         if hub is None:
             hub = get_hub()
@@ -20,16 +22,16 @@ class Resolver(object):
     # below are thread-safe in Python, even if they are not thread-safe in C.
 
     def gethostbyname(self, *args):
-        return self.pool.apply(_socket.gethostbyname, args)
+        return self.pool.apply_e(self.expected_errors, _socket.gethostbyname, args)
 
     def gethostbyname_ex(self, *args):
-        return self.pool.apply(_socket.gethostbyname_ex, args)
+        return self.pool.apply_e(self.expected_errors, _socket.gethostbyname_ex, args)
 
     def getaddrinfo(self, *args, **kwargs):
-        return self.pool.apply(_socket.getaddrinfo, args, kwargs)
+        return self.pool.apply_e(self.expected_errors, _socket.getaddrinfo, args, kwargs)
 
     def gethostbyaddr(self, *args, **kwargs):
-        return self.pool.apply(_socket.gethostbyaddr, args, kwargs)
+        return self.pool.apply_e(self.expected_errors, _socket.gethostbyaddr, args, kwargs)
 
     def getnameinfo(self, *args, **kwargs):
-        return self.pool.apply(_socket.getnameinfo, args, kwargs)
+        return self.pool.apply_e(self.expected_errors, _socket.getnameinfo, args, kwargs)
