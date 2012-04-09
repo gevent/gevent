536793df69b5110a00c6b30fc33c063679c6e8e9
Denis Bilenko <denis.bilenko@gmail.com>
2012-04-09 21:08:09 +0400

    coros.py: add a comment

--- gevent/coros.py
+++ gevent/coros.py
@@ -1,3 +1,4 @@
+# This module definitely remains in 1.0.x, probably in versions after that too.
 import warnings
 warnings.warn('gevent.coros has been renamed to gevent.lock', DeprecationWarning, stacklevel=2)
 
