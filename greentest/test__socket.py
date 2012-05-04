b00e23d5eb64d9deb2f86b21a2b5762b72094ad0
Denis Bilenko <denis.bilenko@gmail.com>
2012-05-04 18:41:59 +0400

    test__socket.py: add TestRef

--- greentest/test__socket.py
+++ greentest/test__socket.py
@@ -199,5 +199,16 @@ class TestClosedSocket(greentest.TestCase):
                 raise
 
 
+class TestRef(greentest.TestCase):
+
+    def test(self):
+        sock = socket.socket()
+        assert sock.ref is True, sock.ref
+        sock.ref = False
+        assert sock.ref is False, sock.ref
+        assert sock._read_event.ref is False, sock.ref
+        assert sock._write_event.ref is False, sock.ref
+
+
 if __name__ == '__main__':
     greentest.main()
