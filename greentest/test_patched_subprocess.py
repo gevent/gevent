cc498247aa7f0da534989f81395e21e87335f90e
Denis Bilenko <denis.bilenko@gmail.com>
2012-05-08 16:44:57 +0400

    test_patched_subprocess.py: remove temp file, check monkey patching

--- greentest/test_patched_subprocess.py
+++ greentest/test_patched_subprocess.py
@@ -1,2 +1,9 @@
+import os
+import glob
 import helper
-exec helper.prepare_stdlib_test(__file__) in globals()
+before = set(glob.glob('@test_*_tmp'))
+helper.run(__file__, globals())
+for filename in set(glob.glob('@test_*_tmp')) - before:
+    os.unlink(filename)
+import subprocess
+assert 'gevent' in repr(subprocess.Popen.__init__.im_func.func_code), repr(subprocess.Popen.__init__.im_func.func_code)
