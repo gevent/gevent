a3295c9c7df0d69ba4d2722c968d38fda26ddda4
Denis Bilenko <denis.bilenko@gmail.com>
2012-08-29 20:29:31 +0400

    disable test_signal.SiginterruptTest.test_without_siginterrupt and test_siginterrupt_on because they rely on os.read raising EINTR which gevent.os.read does not do

--- greentest/patched_tests_setup.py
+++ greentest/patched_tests_setup.py
@@ -110,6 +110,10 @@ disabled_tests = \
     # gevent.select.select() does not get interrupted (unless signal handler raises an error)
     # maybe it should?
 
+    , 'test_signal.SiginterruptTest.test_without_siginterrupt'
+    , 'test_signal.SiginterruptTest.test_siginterrupt_on'
+    # these rely on os.read raising EINTR which never happens with gevent.os.read
+
     , 'test_subprocess.test_leak_fast_process_del_killed'
     , 'test_subprocess.test_zombie_fast_process_del'
     # relies on subprocess._active which we don't use
