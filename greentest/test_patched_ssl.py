c69c5b9c75671aa61b5cba7f0bb758ff0ff6b8eb
Denis Bilenko <denis.bilenko@gmail.com>
2012-09-15 01:46:06 +0400

    remove test_patched_*.py
    it is not handled by test__monkey_patching.py

--- greentest/test_patched_ssl.py
+++
@@ -1,2 +0,0 @@
-import helper
-helper.run(__file__, globals(), '*.pem')
--- greentest/update_patched_tests.py
+++
@@ -1,7 +0,0 @@
-import os
-import glob
-
-for filename in glob.glob('test_patched_*.py'):
-    cmd = 'cp patched_test.py %s' % filename
-    print cmd
-    os.system(cmd)
