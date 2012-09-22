96de78565590abef6500bd20f92c2218fb48057b
Denis Bilenko <denis.bilenko@gmail.com>
2012-09-23 02:24:57 +0400

    update xtest_pep8.py

--- greentest/xtest_pep8.py
+++ greentest/xtest_pep8.py
@@ -1,75 +1,26 @@
 import sys
 import os
-import unittest
-import glob
-import re
-from os.path import abspath, dirname, join, basename
-
-# this regex matches filenames of the standard tests (with one underscore)
-stdtest_re = re.compile('^(test_[^_].+|lock_tests)\.py$')
-
-script = 'pep8'
-
-try:
-    index = sys.argv.index('--script')
-except ValueError:
-    pass
-else:
-    script = sys.argv[index + 1]
-    del sys.argv[index:index + 2]
-
-
-# E501 line too long (80 character limit that we don't follow)
-command = script + ' --repeat --statistics --ignore E501 %s | grep -v E501'
 
 
 if os.system('pep8 --version'):
-    sys.stderr.write('Please install pep8 script\n')
-    sys.exit(0)
+    sys.exit('ERROR: pep8 script not found')
 
 
-def system(*args):
-    command = ' '.join(args)
-    popen_result = os.popen(command)
-    result = popen_result.read()
-    if result:
-        sys.stderr.write(result)
-        raise AssertionError('"%s" failed' % str(command)[:100])
+if not os.path.exists('gevent') and not os.path.exists('setup.py'):
+    os.chdir('..')
 
 
-class Test(unittest.TestCase):
-
-    def test_gevent(self):
-        import gevent
-        # E221 multiple spaces before operator
-        system(command % abspath(dirname(gevent.__file__)), '| grep -v E221')
-
-    def test_tests(self):
-        # E702 multiple statements on one line (from gevent import monkey; monkey.patch_all())
-        files = glob.glob(join(abspath(dirname(__file__)), '*.py'))
-        # filter out standard tests of form test_xxx.py (one underscore)
-        files = [filename for filename in files if stdtest_re.match(basename(filename)) is None]
-        system(command % (' '.join(files)), '| grep -v E702')
-
-    # we keep the standard tests as close to the originals as possible, so don't test them
-    def X_test_std_tests(self):
-        # E702 multiple statements on one line (from gevent import monkey; monkey.patch_all())
-        files = glob.glob(join(abspath(dirname(__file__)), '*.py'))
-        # only count standard tests of form test_xxx.py (one underscore)
-        files = [filename for filename in files if stdtest_re.match(basename(filename)) is not None]
-        system(command % (' '.join(files)), '| grep -v E702')
-
-    def test_examples(self):
-        # E702 multiple statements on one line (from gevent import monkey; monkey.patch_all())
-        # E202 whitespace before '('
-        system(command % join(dirname(abspath(dirname(__file__))), 'examples'), '| grep -v E702 | grep -v E202')
-
-    def test_doc(self):
-        system(command % join(dirname(abspath(dirname(__file__))), 'doc'))
-
-    def test_setup(self):
-        system(command % join(dirname(abspath(dirname(__file__))), 'setup.py'))
+commands = [
+    'pep8 --show-source --max-line-length=160 gevent/ setup.py',
+    'pep8 --show-source --max-line-length=160 --ignore E702 examples/*.py',
+    'pep8 --show-source --max-line-length=160 --ignore E702,E128 examples/webchat',
+    'pep8 --show-source --max-line-length=160 doc/']
 
 
-if __name__ == '__main__':
-    unittest.main()
+failures = 0
+for command in commands:
+    if os.system(command):
+        failures += 1
+
+
+sys.exit(failures)
