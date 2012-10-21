b9ceb311b744a7335ef7dba2ada6085cbb37d6fc
Denis Bilenko <denis.bilenko@gmail.com>
2012-10-21 22:09:47 +0200

    test__order.py: add tests for sleep(0) order

--- greentest/test__order.py
+++ greentest/test__order.py
@@ -40,5 +40,24 @@ class TestM(Test):
     count = 1000
 
 
+class TestSleep0(greentest.TestCase):
+
+    def test(self):
+        lst = []
+        gevent.spawn(sleep0, lst, '1')
+        gevent.spawn(sleep0, lst, '2')
+        gevent.wait()
+        self.assertEqual(' '.join(lst), '1A 2A 1B 2B')
+
+
+def sleep0(lst, param):
+    lst.append(param + 'A')
+    gevent.sleep(0)
+    lst.append(param + 'B')
+
+
+del TestSleep0  # XXX
+
+
 if __name__ == '__main__':
     greentest.main()
