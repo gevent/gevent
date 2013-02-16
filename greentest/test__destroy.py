67a8dac80744ae9cdd5fd423866194c257c95154
Denis Bilenko <denis.bilenko@gmail.com>
2013-02-16 17:58:18 +0100

    core: check for loop._ptr being NULL
    raise ValueError if it is. Previously it crashed.
    
    Closes #251. Thanks to Jan-Philip Gehrcke.

--- greentest/test__destroy.py
+++ greentest/test__destroy.py
@@ -8,14 +8,19 @@ hub.destroy()
 hub = gevent.get_hub()
 assert hub.loop.default, hub
 
+saved_loop = hub.loop
 # Destroy hub including default loop.
 hub.destroy(destroy_loop=True)
+assert saved_loop.fileno() is None, saved_loop
+print hub, saved_loop
+
 # Create new hub and explicitly request creation of a new default loop.
 hub = gevent.get_hub(default=True)
 assert hub.loop.default, hub
 
 # Destroy hub including default loop.
 hub.destroy(destroy_loop=True)
+
 # Create new non-default loop in new hub.
 hub = gevent.get_hub()
 assert not hub.loop.default, hub
