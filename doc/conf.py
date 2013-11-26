3d6e61748255b57d560856e911e10f3cd73c23da
Denis Bilenko <denis.bilenko@gmail.com>
2013-11-26 10:19:32 +0100

    add gevent.org theme

--- doc/conf.py
+++ doc/conf.py
@@ -108,7 +108,8 @@ modindex_common_prefix = ['gevent.']
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'mytheme'
+html_theme_path = ['.']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
