da8ac8c66c2a6405f4cb89251d5ea4758872f1e2
Denis Bilenko <denis.bilenko@gmail.com>
2013-12-01 12:14:54 +0100

    set version to 1.1dev

--- gevent/__init__.py
+++ gevent/__init__.py
@@ -8,8 +8,8 @@ See http://www.gevent.org/ for the documentation.
 
 from __future__ import absolute_import
 
-version_info = (1, 0, 0, 'dev', None)
-__version__ = '1.0'
+version_info = (1, 1, 0, 'dev', None)
+__version__ = '1.1'
 
 
 __all__ = ['get_hub',
