5482db66bdf37874ce9ad6d805006ec74614ec17
Denis Bilenko <denis.bilenko@gmail.com>
2013-12-01 18:53:07 +0100

    cythonpp.py: drop support for python2.5

--- util/cythonpp.py
+++ util/cythonpp.py
@@ -8,6 +8,7 @@ import datetime
 import pipes
 import difflib
 from hashlib import md5
+from itertools import combinations, product
 
 do_exec = None
 if sys.version_info >= (3, 0):
@@ -723,41 +724,6 @@ def dbg(*args):
     if not DEBUG:
         return
     return log(*args)
-
-
-# itertools is not available on python 2.5
-# itertools.combinations has a problem on 2.6.1
-
-def combinations(iterable, r):
-    # combinations('ABCD', 2) --> AB AC AD BC BD CD
-    # combinations(range(4), 3) --> 012 013 023 123
-    pool = tuple(iterable)
-    n = len(pool)
-    if r > n:
-        return
-    indices = list(range(r))
-    yield tuple(pool[i] for i in indices)
-    while True:
-        for i in reversed(range(r)):
-            if indices[i] != i + n - r:
-                break
-        else:
-            return
-        indices[i] += 1
-        for j in range(i + 1, r):
-            indices[j] = indices[j - 1] + 1
-        yield tuple(pool[i] for i in indices)
-
-
-def product(*args, **kwds):
-    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-    pools = tuple(map(tuple, args)) * kwds.get('repeat', 1)
-    result = [[]]
-    for pool in pools:
-        result = [x + [y] for x in result for y in pool]
-    for prod in result:
-        yield tuple(prod)
 
 
 if __name__ == '__main__':
