5ca76f53f65c3e74adf5c0663bf01ff288cb090e
Fantix King <fantix.king@gmail.com>
2013-12-19 19:46:56 +0800

    Handle reraise in PY3

--- gevent/hub.py
+++ gevent/hub.py
@@ -34,10 +34,36 @@ if PY3:
     string_types = str,
     integer_types = int,
     xrange = range
+
+    import builtins
+    exec_ = getattr(builtins, "exec")
+
+    def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+
+    del builtins
 else:
     string_types = basestring,
     integer_types = (int, long)
     xrange = xrange
+
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+
+    exec_("""def reraise(tp, value, tb=None):
+    raise tp, value, tb
+""")
 
 
 if sys.version_info[0] <= 2:
