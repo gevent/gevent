f3810691f6a44181f1bf5d0e67d177d52f6fa160
Fantix King <fantix.king@gmail.com>
2014-02-21 18:33:08 +0800

    Fix increased refcount in select.py

--- gevent/select.py
+++ gevent/select.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from gevent.event import Event
 from gevent.hub import get_hub
 from gevent.hub import integer_types
+from gevent.hub import PY3
 
 __implements__ = ['select']
 __all__ = ['error'] + __implements__
@@ -14,7 +15,9 @@ error = __select__.error
 def get_fileno(obj):
     try:
         fileno_f = obj.fileno
-    except AttributeError:
+    except AttributeError as ex:
+        if PY3:
+            ex.__traceback__ = None
         if not isinstance(obj, integer_types):
             raise TypeError('argument must be an int, or have a fileno() method: %r' % (obj, ))
         return obj
