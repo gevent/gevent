166dc3c78322c36c4f337d5baa1448298822b21d
Fantix King <fantix.king@gmail.com>
2014-03-05 11:55:27 +0800

    Fix gevent.socket.socket.type API impl for PY3

--- gevent/_socket3.py
+++ gevent/_socket3.py
@@ -46,7 +46,10 @@ class socket(_socket.socket):
 
     @property
     def type(self):
-        return _socket.socket.type.__get__(self) & ~_socket.SOCK_NONBLOCK
+        if self.timeout != 0.0 and hasattr(_socket, 'SOCK_NONBLOCK'):
+            return _socket.socket.type.__get__(self) & ~_socket.SOCK_NONBLOCK
+        else:
+            return _socket.socket.type.__get__(self)
 
     def __enter__(self):
         return self
