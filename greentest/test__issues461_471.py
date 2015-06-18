5585b61b579d48a08714f66ec1e9aa82aa7142c1
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2015-06-18 11:36:14 -0700

    Fix test for issues #461 and #471 on Python 2.

--- greentest/test__issues461_471.py
+++ greentest/test__issues461_471.py
@@ -10,6 +10,7 @@ import sys
 
 if sys.argv[1:] == ['subprocess']:
     import gevent
+
     def task():
         sys.stdout.write('ready\n')
         sys.stdout.flush()
@@ -20,7 +21,9 @@ if sys.argv[1:] == ['subprocess']:
         pass
 else:
     import signal
-    from subprocess import Popen, PIPE, TimeoutExpired
+    from subprocess import Popen, PIPE
+    import time
+
     if sys.platform.startswith('win'):
         from subprocess import CREATE_NEW_PROCESS_GROUP
         kwargs = {'creationflags': CREATE_NEW_PROCESS_GROUP}
@@ -29,9 +32,11 @@ else:
     p = Popen([sys.executable, __file__, 'subprocess'], stdout=PIPE, **kwargs)
     p.stdout.readline()
     p.send_signal(signal.SIGINT)
-    try:
-        p.wait(3)
-    except TimeoutExpired:
+    for i in range(30):
+        if p.poll() is not None:
+            break
+        time.sleep(0.1)
+    else:
         p.terminate()
         sys.exit(1)
     sys.exit(p.returncode)
