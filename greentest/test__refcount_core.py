0a92b17e5f65fcad1bc9b08e7218d9b189e9a8e6
Jason Madden <jamadden@gmail.com>
2015-06-25 17:22:01 -0500

    Whoops, misread the line number. [skip ci]

--- greentest/test__refcount_core.py
+++ greentest/test__refcount_core.py
@@ -5,12 +5,11 @@ class Dummy:
     def __init__(self):
         __import__('gevent.core')
 
-
-assert weakref.ref(Dummy())() is None
-
-from gevent import socket
-
 try:
+    assert weakref.ref(Dummy())() is None
+
+    from gevent import socket
+
     assert weakref.ref(socket.socket())() is None
 except AssertionError:
     import sys
