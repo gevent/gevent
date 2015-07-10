15560a83be7614f76df443fab9f72c87a1e004fa
Heungsub Lee <sub@subl.ee>
2015-07-10 12:51:40 +0900

    More test cases to prevent side-effect of #607 fix

--- greentest/test__issue607.py
+++ greentest/test__issue607.py
@@ -7,16 +7,30 @@ class ExpectedError(greentest.ExpectedException):
     pass
 
 
+def f():
+    gevent.sleep(999)
+
+
 class TestKillWithException(greentest.TestCase):
 
     def test_kill_without_exception(self):
-        g = gevent.spawn()
+        g = gevent.spawn(f)
         g.kill()
         assert g.successful()
         assert isinstance(g.get(), gevent.GreenletExit)
 
     def test_kill_with_exception(self):
-        g = gevent.spawn()
+        # issue-607 pointed this case.
+        g = gevent.spawn(f)
+        g.kill(ExpectedError)
+        assert not g.successful()
+        self.assertRaises(ExpectedError, g.get)
+        assert g.value is None
+        assert isinstance(g.exception, ExpectedError)
+
+    def test_kill_with_exception_after_started(self):
+        g = gevent.spawn(f)
+        g.join(0)
         g.kill(ExpectedError)
         assert not g.successful()
         self.assertRaises(ExpectedError, g.get)
