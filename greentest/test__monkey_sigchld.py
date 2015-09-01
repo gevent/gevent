6502841d8d4d12760aa1d031b758c0852a664c6d
Jason Madden <jamadden@gmail.com>
2015-09-01 08:11:38 -0500

    Don't try to run SIGCHLD test on win32

--- greentest/test__monkey_sigchld.py
+++ greentest/test__monkey_sigchld.py
@@ -19,18 +19,21 @@ def handle_sigchld(*args):
     raise TypeError("This should be ignored but printed")
 
 import signal
-assert signal.getsignal(signal.SIGCHLD) == signal.SIG_DFL
-signal.signal(signal.SIGCHLD, handle_sigchld)
-handler = signal.getsignal(signal.SIGCHLD)
-assert signal.getsignal(signal.SIGCHLD) is handle_sigchld, handler
+if hasattr(signal, 'SIGCHLD'):
+    assert signal.getsignal(signal.SIGCHLD) == signal.SIG_DFL
+    signal.signal(signal.SIGCHLD, handle_sigchld)
+    handler = signal.getsignal(signal.SIGCHLD)
+    assert signal.getsignal(signal.SIGCHLD) is handle_sigchld, handler
 
-pid = os.fork()
-if not pid:
-    # child
-    gevent.sleep(0.2)
-    sys.exit(0)
-else:
-    with gevent.Timeout(1):
-        while awaiting_child:
-            gevent.sleep(0.01)
+    pid = os.fork()
+    if not pid:
+        # child
+        gevent.sleep(0.2)
         sys.exit(0)
+    else:
+        with gevent.Timeout(1):
+            while awaiting_child:
+                gevent.sleep(0.01)
+            sys.exit(0)
+else:
+    print("No SIGCHLD, not testing")
