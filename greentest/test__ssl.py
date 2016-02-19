a47a676b583486ffdba2d64ea91f1500883876ec
Jason Madden <jamadden@gmail.com>
2016-02-19 07:10:54 -0600

    pypy 'incorrectly' raises a plain ssl.SSLError where CPython would raise a ssl.SSLEOFError, even though pypy defines that class. But pypy doesn't try too hard to match exact exceptions with cpython.

--- greentest/test__ssl.py
+++ greentest/test__ssl.py
@@ -57,13 +57,15 @@ class TestSSL(test__socket.TestTCP):
                 server_sock[0][0].close()
 
         def test_empty_send(self):
-            # Sending empty bytes with the 'send' method
-            # raises ssl.SSLEOFError in the stdlib. (just ssl.SSLError on
-            # py26). Issue 719
-            expected = getattr(ssl, 'SSLEOFError', ssl.SSLError)
+            # Issue 719
+            # Sending empty bytes with the 'send' method raises
+            # ssl.SSLEOFError in the stdlib. PyPy 4.0 and CPython 2.6
+            # both just raise the superclass, ssl.SSLError.
+            expected = ssl.SSLError
             self.assertRaises(expected, self._test_sendall,
                               b'',
                               client_method='send')
+
 
 def ssl_listener(address, private_key, certificate):
     raw_listener = socket.socket()
