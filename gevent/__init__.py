10c1554bc01df4674c717c401bde04d89733d0f4
Jason Madden <jamadden@gmail.com>
2016-03-05 10:20:03 -0600

    update version [skip ci]

--- gevent/__init__.py
+++ gevent/__init__.py
@@ -15,10 +15,10 @@ _version_info = namedtuple('version_info',
 
 #: The programatic version identifier. The fields have (roughly) the
 #: same meaning as :data:`sys.version_info`
-version_info = _version_info(1, 1, 0, 'final', 0)
+version_info = _version_info(1, 2, 0, 'alpha', 0)
 
 #: The human-readable PEP 440 version identifier
-__version__ = '1.1.0'
+__version__ = '1.2.0.dev0'
 
 
 __all__ = ['get_hub',
