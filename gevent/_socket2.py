cbeef54aabdc93bf944639eced2ddf94fed1edc3
Jason Madden <jamadden@gmail.com>
2016-03-21 09:45:28 -0500

    remove code for 2.6 support

--- gevent/_socket2.py
+++ gevent/_socket2.py
@@ -53,22 +53,19 @@ else:
                 if not self.closed:
                     self.close()
 
-if sys.version_info[:2] < (2, 7):
-    _get_memory = buffer
-else:
-    def _get_memory(data):
-        try:
-            mv = memoryview(data)
-            if mv.shape:
-                return mv
-            # No shape, probably working with a ctypes object,
-            # or something else exotic that supports the buffer interface
-            return mv.tobytes()
-        except TypeError:
-            # fixes "python2.7 array.array doesn't support memoryview used in
-            # gevent.socket.send" issue
-            # (http://code.google.com/p/gevent/issues/detail?id=94)
-            return buffer(data)
+def _get_memory(data):
+    try:
+        mv = memoryview(data)
+        if mv.shape:
+            return mv
+        # No shape, probably working with a ctypes object,
+        # or something else exotic that supports the buffer interface
+        return mv.tobytes()
+    except TypeError:
+        # fixes "python2.7 array.array doesn't support memoryview used in
+        # gevent.socket.send" issue
+        # (http://code.google.com/p/gevent/issues/detail?id=94)
+        return buffer(data)
 
 
 class _closedsocket(object):
