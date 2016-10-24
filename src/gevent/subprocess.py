eaa71b385c0317ebd817bb1da8435c0f8220ede1
Jason Madden <jamadden@gmail.com>
2016-10-24 11:20:59 -0500

    Documentation and re-enable all test platforms. Fixes #748.

--- src/gevent/subprocess.py
+++ src/gevent/subprocess.py
@@ -12,11 +12,13 @@ Cooperative ``subprocess`` module.
    signal is sent to a different thread.
 
 .. note:: The interface of this module is intended to match that of
-   the standard library :mod:`subprocess` module. There are some small
-   differences between the Python 2 and Python 3 versions of that
-   module and between the POSIX and Windows versions. The HTML
-   documentation here can only describe one version; for definitive
-   documentation, see the standard library or the source code.
+   the standard library :mod:`subprocess` module (with many backwards
+   compatible extensions from Python 3 backported to Python 2). There
+   are some small differences between the Python 2 and Python 3
+   versions of that module and between the POSIX and Windows versions.
+   The HTML documentation here can only describe one version; for
+   definitive documentation, see the standard library or the source
+   code.
 
 .. _is not defined: http://www.linuxprogrammingblog.com/all-about-linux-signals?page=11
 """
