999838bf42778b07a4ac3577eff9b3830e5b3372
Josh Snyder <joshs@netflix.com>
2018-10-16 00:13:12 +0000

    Add a test showing that WaitIterator will return the same object many times
    If someone calls `gevent.iwait([event1,...79 other objects])`, the iterator
    will yield 80 objects, and (based on how frequently each of the objects becomes
    ready), could potentially yield `event1` every time. I imagine this would catch
    most users off-guard, especially because the number of other objects waited upon
    (79, in this case) determines the number of times iwait will yield.
    
        FAIL: test_wait_unique (__main__.TestWaiting)
        ----------------------------------------------------------------------
        Traceback (most recent call last):
          File "/home/vagrant/gevent/src/greentest/greentest/leakcheck.py", line 200, in wrapper
            return _RefCountChecker(self, method)(args, kwargs)
          File "/home/vagrant/gevent/src/greentest/greentest/leakcheck.py", line 184, in __call__
            self._run_test(args, kwargs)
          File "/home/vagrant/gevent/src/greentest/greentest/leakcheck.py", line 109, in _run_test
            self.function(self.testcase, *args, **kwargs)
          File "/home/vagrant/gevent/src/greentest/greentest/errorhandler.py", line 48, in wrapper
            return method(self, *args, **kwargs)
          File "/home/vagrant/gevent/src/greentest/greentest/errorhandler.py", line 35, in wrapper
            return method(self, *args, **kwargs)
          File "/home/vagrant/gevent/src/greentest/greentest/testcase.py", line 63, in wrapper
            return method(self, *args, **kwargs)
          File "src/greentest/test__wait.py", line 19, in test_wait_unique
            self.assertEqual(waited_objs, set([sem1, sem2]))
        AssertionError: Items in the second set but not the first:
        <gevent.__semaphore.Semaphore object at 0x7fa576932c50>

--- src/greentest/test__wait.py
+++ src/greentest/test__wait.py
@@ -12,6 +12,22 @@ class TestWaiting(greentest.TestCase):
         ready = next(gevent.iwait((sem1, sem2)))
         self.assertEqual(sem1, ready)
 
+    def test_wait_unique(self):
+        sem1 = Semaphore()
+        sem2 = Semaphore()
+
+        def release():
+            for i in range(2):
+                sem1.release()
+                gevent.idle()
+            sem2.release()
+
+        glet = gevent.spawn(release)
+        waited_objs = set(gevent.iwait((sem1, sem2)))
+        self.assertEqual(waited_objs, set([sem1, sem2]))
+        glet.kill()
+        glet.get()
+
 
 if __name__ == '__main__':
         greentest.main()
