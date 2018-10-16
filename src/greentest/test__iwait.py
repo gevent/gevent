141ba1589c68c38c3efb7f10702dccb924f677ee
Jason Madden <jamadden@gmail.com>
2018-10-16 12:02:06 -0500

    Rename test_wait to test__iwait and add to the relevant .txt files.

--- src/greentest/test__iwait.py
+++ src/greentest/test__iwait.py
@@ -3,10 +3,11 @@ import greentest
 from gevent.lock import Semaphore
 
 
-class TestWaiting(greentest.TestCase):
-    def test_wait_noiter(self):
-        """Test that gevent.iwait returns objects which can be iterated upon
-        without additional calls to iter()"""
+class Testiwait(greentest.TestCase):
+
+    def test_noiter(self):
+        # Test that gevent.iwait returns objects which can be iterated upon
+        # without additional calls to iter()
 
         sem1 = Semaphore()
         sem2 = Semaphore()
@@ -14,3 +15,6 @@ class TestWaiting(greentest.TestCase):
         gevent.spawn(sem1.release)
         ready = next(gevent.iwait((sem1, sem2)))
         self.assertEqual(sem1, ready)
+
+if __name__ == '__main__':
+    greentest.main()
