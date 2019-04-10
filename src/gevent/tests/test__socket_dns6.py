fad6b21f81546bb78f51fcc0136367c4ad3b3388
Jason Madden <jamadden@gmail.com>
2019-04-10 11:53:30 -0500

    socket/dns test cleanups for CI. Use test.support.find_unused_port() instead of rolling our own; it handles windows better. Limit the DNS6 tests to known good configurations. Hopefully fix a bunch of flakiness in test__refcount's use of sockets.
    Fixes #1389

--- src/gevent/tests/test__socket_dns6.py
+++ src/gevent/tests/test__socket_dns6.py
@@ -2,79 +2,92 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, absolute_import, division
 
-import gevent.testing as greentest
 import socket
+import unittest
+
+import gevent.testing as greentest
 from gevent.tests.test__socket_dns import TestCase, add
 
+from gevent.testing.sysinfo import OSX
 from gevent.testing.sysinfo import RESOLVER_NOT_SYSTEM
 from gevent.testing.sysinfo import RESOLVER_DNSPYTHON
 
-if not greentest.RUNNING_ON_CI and not RESOLVER_DNSPYTHON:
 
 
-    # We can't control the DNS servers we use there
-    # for the system. This works best with the google DNS servers
-    # The getnameinfo test can fail on CI.
+# We can't control the DNS servers on CI (or in general...)
+# for the system. This works best with the google DNS servers
+# The getnameinfo test can fail on CI.
 
-    # Previously only Test6_ds failed, but as of Jan 2018, Test6
-    # and Test6_google begin to fail:
+# Previously only Test6_ds failed, but as of Jan 2018, Test6
+# and Test6_google begin to fail:
 
-      # First differing element 0:
-    # 'vm2.test-ipv6.com'
-    # 'ip119.gigo.com'
+# First differing element 0:
+# 'vm2.test-ipv6.com'
+# 'ip119.gigo.com'
 
-    # - ('vm2.test-ipv6.com', [], ['2001:470:1:18::125'])
-    # ?   ---------  ^^                             ^^
+# - ('vm2.test-ipv6.com', [], ['2001:470:1:18::125'])
+# ?   ---------  ^^                             ^^
 
-    # + ('ip119.gigo.com', [], ['2001:470:1:18::119'])
-    # ?     ^^^^^^^^                             ^^
+# + ('ip119.gigo.com', [], ['2001:470:1:18::119'])
+# ?     ^^^^^^^^                             ^^
 
-    class Test6(TestCase):
+# These are known to work on jamadden's OS X machine using the google
+# resolvers (but not with DNSPython; things don't *quite* match)...so
+# by default we skip the tests everywhere else.
 
-        # host that only has AAAA record
-        host = 'aaaa.test-ipv6.com'
+class Test6(TestCase):
 
-        def test_empty(self):
-            self._test('getaddrinfo', self.host, 'http')
+    # host that only has AAAA record
+    host = 'aaaa.test-ipv6.com'
 
-        def test_inet(self):
-            self._test('getaddrinfo', self.host, None, socket.AF_INET)
+    if not OSX or RESOLVER_DNSPYTHON:
+        def _test(self, *args): # pylint:disable=arguments-differ
+            raise unittest.SkipTest(
+                "Only known to work on jamadden's machine. "
+                "Please help investigate and make DNS tests more robust."
+            )
 
-        def test_inet6(self):
-            self._test('getaddrinfo', self.host, None, socket.AF_INET6)
+    def test_empty(self):
+        self._test('getaddrinfo', self.host, 'http')
 
-        def test_unspec(self):
-            self._test('getaddrinfo', self.host, None, socket.AF_UNSPEC)
+    def test_inet(self):
+        self._test('getaddrinfo', self.host, None, socket.AF_INET)
 
+    def test_inet6(self):
+        self._test('getaddrinfo', self.host, None, socket.AF_INET6)
 
-    class Test6_google(Test6):
-        host = 'ipv6.google.com'
-
-        def _normalize_result_getnameinfo(self, result):
-            if greentest.RUNNING_ON_CI and RESOLVER_NOT_SYSTEM:
-                # Disabled, there are multiple possibilities
-                # and we can get different ones, rarely.
-                return ()
-            return result
-
-    add(Test6, Test6.host)
-    add(Test6_google, Test6_google.host)
+    def test_unspec(self):
+        self._test('getaddrinfo', self.host, None, socket.AF_UNSPEC)
 
 
+class Test6_google(Test6):
+    host = 'ipv6.google.com'
 
-    class Test6_ds(Test6):
-        # host that has both A and AAAA records
-        host = 'ds.test-ipv6.com'
-
-        def _normalize_result_gethostbyaddr(self, result):
-            # This test is effectively disabled. There are multiple address
-            # that resolve and which ones you get depend on the settings
-            # of the system and ares. They don't match exactly.
+    def _normalize_result_getnameinfo(self, result):
+        if greentest.RUNNING_ON_CI and RESOLVER_NOT_SYSTEM:
+            # Disabled, there are multiple possibilities
+            # and we can get different ones, rarely.
             return ()
+        return result
 
-        _normalize_result_gethostbyname = _normalize_result_gethostbyaddr
+add(Test6, Test6.host)
+add(Test6_google, Test6_google.host)
 
-    add(Test6_ds, Test6_ds.host)
+
+
+class Test6_ds(Test6):
+    # host that has both A and AAAA records
+    host = 'ds.test-ipv6.com'
+
+    def _normalize_result_gethostbyaddr(self, result):
+        # This test is effectively disabled. There are multiple address
+        # that resolve and which ones you get depend on the settings
+        # of the system and ares. They don't match exactly.
+        return ()
+
+    _normalize_result_gethostbyname = _normalize_result_gethostbyaddr
+
+add(Test6_ds, Test6_ds.host)
 
 
 if __name__ == '__main__':
