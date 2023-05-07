036fddf9c4dd84dd516d5b4258f97085b5a21ce6
Marcel Telka <marcel@telka.sk>
2023-05-07 23:53:08 +0200

    Fix asserts in test_attributes

--- src/gevent/tests/test__socket.py
+++ src/gevent/tests/test__socket.py
@@ -396,13 +396,13 @@ class TestTCP(greentest.TestCase):
 
     def test_attributes(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        self.assertEqual(socket.AF_INET, s.type)
-        self.assertEqual(socket.SOCK_DGRAM, s.family)
+        self.assertEqual(socket.AF_INET, s.family)
+        self.assertEqual(socket.SOCK_DGRAM, s.type)
         self.assertEqual(0, s.proto)
 
         if hasattr(socket, 'SOCK_NONBLOCK'):
             s.settimeout(1)
-            self.assertEqual(socket.AF_INET, s.type)
+            self.assertEqual(socket.AF_INET, s.family)
 
             s.setblocking(0)
             std_socket = monkey.get_original('socket', 'socket')(socket.AF_INET, socket.SOCK_DGRAM, 0)
